16b0846e6c44905bf300451506fc22b4f520d792
Christian Schmitt <christian.schmitt@cs.fau.de>
2013-11-27 15:17:06 +0100

    Missing file for previous commit

---
+++ Compiler/src/exastencils/datastructures/l4/Traits.scala
@@ -0,0 +1,14 @@
+package exastencils.datastructures.l4
+
+
+trait CppPrettyPrintable {
+  def cpp() : String
+}
+
+trait CudaPrettyPrintable {
+  def cuda() : String
+}
+
+trait Debuggable {
+  def debug() : String
+}
