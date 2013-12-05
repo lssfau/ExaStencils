c30155fb210b21e0f931c87d7eb2dc0462d50c57
Christian Schmitt <christian.schmitt@cs.fau.de>
2013-12-05 14:20:33 +0100

    copied level 4 datastructures to IR as a working base

---
+++ Compiler/src/exastencils/datastructures/ir/Expressions.scala
@@ -0,0 +1,48 @@
+package exastencils.datastructures.ir
+
+import exastencils.datastructures._
+import exastencils.datastructures.ir._
+
+trait Expression extends Node with CppPrettyPrintable {
+  override def duplicate : this.type
+}
+
+case class StringLiteral(value : String) extends Expression {
+  override def cpp = value
+  override def duplicate = this.copy().asInstanceOf[this.type]
+}
+
+case class NumericLiteral(value : Number) extends Expression {
+  override def cpp = value.toString
+  override def duplicate = this.copy().asInstanceOf[this.type]
+}
+
+case class BooleanLiteral(value : Boolean) extends Expression {
+  override def cpp = value.toString
+  override def duplicate = this.copy().asInstanceOf[this.type]
+}
+
+case class Identifier(name : String) extends Expression {
+  override def cpp = name
+  override def duplicate = this.copy().asInstanceOf[this.type]
+}
+
+case class Variable(name : String, Type : Datatype) extends Expression {
+  override def cpp = name
+  override def duplicate = this.copy().asInstanceOf[this.type]
+}
+
+case class Constant(value : Any) extends Expression {
+  override def cpp = ""
+  override def duplicate = this.copy().asInstanceOf[this.type]
+}
+
+case class BinaryExpression(operator : String, left : Expression, right : Expression) extends Expression {
+  override def cpp = left.cpp + operator + right.cpp
+  override def duplicate = this.copy(left = Duplicate(left), right = Duplicate(right)).asInstanceOf[this.type]
+}
+
+case class FunctionCall(name : String, arguments : Seq[Expression]) extends Expression {
+  override def cpp = name + "FIXME args"
+  override def duplicate = this.copy(arguments = Duplicate(arguments)).asInstanceOf[this.type]
+}
