45adfcb327c0fea13568a8eb08d600b0d24a2486
kuckuk <sebastian.kuckuk@fau.de>
2013-12-05 15:16:05 +0100

    - moved Fragment strategies to a separate file

---
+++ Compiler/src/exastencils/primitives/Strategies.scala
@@ -0,0 +1,52 @@
+package exastencils.primitives
+
+import exastencils.core._
+import exastencils.datastructures._
+import exastencils.primitives._
+
+object GenCommCode extends (() => Unit) {
+  def apply() : Unit = {
+    var fragment = new FragmentClass;
+
+    println("Setting up FragmentClass");
+
+    // HACK
+    StateManager.root_ = fragment;
+
+    StateManager.apply(new Transformation({
+      case frag : FragmentClass =>
+        for (neigh <- frag.neighbors) {
+          neigh.addDeclarations(frag);
+        }
+
+        frag.init;
+        Some(frag);
+    }));
+
+    StateManager.apply(new Transformation({
+      case frag : FragmentClass =>
+        frag.fields += new Field("Solution", "solData", "double", "NUM_SOL_SLOTS", true);
+        frag.fields += new Field("Residual", "resData", "double", "1", false);
+        frag.fields += new Field("RHS", "rhsData", "double", "1", false);
+        Some(frag);
+    }));
+
+    StateManager.apply(new Transformation({
+      case frag : FragmentClass =>
+        frag.addSync(frag.fields);
+
+        frag.functions += (new ConnectLocalElement().toString_cpp);
+        frag.functions += (new ConnectRemoteElement().toString_cpp);
+        frag.functions += (new SetupBuffers(frag.fields).toString_cpp);
+        Some(frag);
+    }));
+
+    StateManager.apply(new Transformation({
+      case frag : FragmentClass =>
+        frag.toString_cpp;
+        Some(frag);
+    }));
+
+    println("Done");
+  }
+}
