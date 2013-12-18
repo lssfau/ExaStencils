6ce5608391a023979fc0e20f88ca6f8e0d83ddb8
Christian Schmitt <christian.schmitt@cs.fau.de>
2013-12-18 12:04:17 +0100

    renamed Root.statements to Root.nodes to reflect its datatype

--- Compiler/src/exastencils/datastructures/ir/Root.scala
+++ Compiler/src/exastencils/datastructures/ir/Root.scala
@@ -3,8 +3,8 @@ package exastencils.datastructures.ir
 import exastencils.datastructures._
 import scala.collection.mutable.ListBuffer
 
-case class Root(var statements : List[Node]) extends Node {
+case class Root(var nodes : List[Node]) extends Node {
 	override def duplicate = { //this.copy(statements=ListBuffer()).asInstanceOf[this.type]
-	this.copy(statements = Duplicate(statements)).asInstanceOf[this.type]
+	this.copy(nodes = Duplicate(nodes)).asInstanceOf[this.type]
 	}
 }
