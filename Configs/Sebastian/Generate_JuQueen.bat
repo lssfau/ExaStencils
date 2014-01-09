262d228d25a746edcad60654c832212e325d3ac3
kuckuk <sebastian.kuckuk@fau.de>
2014-01-09 10:37:01 +0100

    - added more JuQueen configurations

--- Configs/Sebastian/Generate_JuQueen.bat
+++ Configs/Sebastian/Generate_JuQueen.bat
@@ -1,4 +1,4 @@
-set configurations=64_6_1_16x16x16_1x1x1 64_26_1_16x16x16_1x1x1 64_6_0_8x8x8_2x2x2 64_26_0_8x8x8_2x2x2 64_6_1_8x8x8_2x2x2 64_26_1_8x8x8_2x2x2
+set configurations=64_6_1_16x16x16_1x1x1 64_26_1_16x16x16_1x1x1 64_6_0_8x8x8_2x2x2 64_26_0_8x8x8_2x2x2 64_6_1_8x8x8_2x2x2 64_26_1_8x8x8_2x2x2 64_6_0_4x4x4_4x4x4 64_26_0_4x4x4_4x4x4 64_6_1_4x4x4_4x4x4 64_26_1_4x4x4_4x4x4
 
 for %%c in (%configurations%) do (
 	java.exe -cp "C:\Eclipse\configuration\org.eclipse.osgi\bundles\286\1\.cp\lib\scala-library.jar;.\Compiler\bin" Main .\Configs\Sebastian\JuQueen_%%c_Settings.txt .\Configs\Sebastian\JuQueen_%%c_Knowledge.txt
