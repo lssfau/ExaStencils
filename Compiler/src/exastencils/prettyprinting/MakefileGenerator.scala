6dd435568a7310b44ec3537c590c219516e8f0d1
kuckuk <sebastian.kuckuk@fau.de>
2014-01-09 09:54:58 +0100

    - fixed MakefileGenerator - extended MakefileGenerator with phony targets and separate targets for each cpp file to allow parallel compiling

--- Compiler/src/exastencils/prettyprinting/MakefileGenerator.scala
+++ Compiler/src/exastencils/prettyprinting/MakefileGenerator.scala
@@ -4,22 +4,40 @@ object MakefileGenerator extends BuildfileGenerator {
   override def write : Unit = {
     val printer = PrettyprintingManager.getPrinter("Makefile")
 
-    printer <<< "CXX = mpic++"
-    printer <<< "CFLAGS = -O3"
+    // TODO: switch by target hardware
+    printer <<< "CXX = mpixlcxx_r"
+    printer <<< "CFLAGS = -O3 -qhot -qarch=qp -qtune=qp -qsmp=omp -DNDEBUG"
+    printer <<< "LFLAGS = -O3 -qhot -qarch=qp -qtune=qp -qsmp=omp -DNDEBUG"	// TODO: check which flags are required
     printer <<< "BINARY = exastencils"
     printer <<< ""
-    printer <<< "exastencils: "
 
-    PrettyprintingManager.getFiles.filter(file => file != "Makefile").foreach(file => {
-      printer << s"$file "
-    })
+    printer <<< ".PHONY: all"
+    printer <<< "all: exastencils"
+    printer <<< ""
 
-    printer <<< "\t${CXX} ${CFLAGS} -o ${BINARY} "
+    printer <<< ".PHONY: clean"
+    printer <<< "clean:"
+    printer << "\trm -f "
+    PrettyprintingManager.getFiles.filter(file => file.endsWith(".cpp")).foreach(file => { printer << s"${file.replace(".cpp", ".o")} " })
+    printer <<< "${BINARY}"
+    printer <<< ""
+
+    printer << "exastencils: "
+    PrettyprintingManager.getFiles.filter(file => file.endsWith(".cpp")).foreach(file => { printer << s"${file.replace(".cpp", ".o")} " })
+    printer <<< ""
+    printer << "\t${CXX} ${LFLAGS} -o ${BINARY} -I. "
+    PrettyprintingManager.getFiles.filter(file => file.endsWith(".cpp")).foreach(file => { printer << s"${file.replace(".cpp", ".o")} " })
+    printer <<< ""
+    printer <<< ""
+
+    // FIXME: header dependencies are not taken into account 
     PrettyprintingManager.getFiles.filter(file => file.endsWith(".cpp")).foreach(file => {
-      printer << s"$file "
+      printer <<< s"${file.replace(".cpp", ".o")}: $file"
+      printer <<< "\t${CXX} ${CFLAGS} -c -o " + file.replace(".cpp", ".o") + " -I. " + file
     })
     printer <<< ""
-    
+    printer <<< ""
+
     printer.close
   }
 }
