78ef6f94dbdd6595278451317c7aa122ca9086b1
kuckuk <sebastian.kuckuk@fau.de>
2014-01-14 13:13:42 +0100

    - added configurations for the LSS cluster

---
+++ Configs/Sebastian/Generate_LSS.bat
@@ -0,0 +1,5 @@
+set configurations=2_6_0_4x4x4_1x1x1 2_26_0_4x4x4_1x1x1 2_6_0_2x2x2_2x2x2 2_26_0_2x2x2_2x2x2 2_6_1_2x2x2_2x2x2 2_26_1_2x2x2_2x2x2
+
+for %%c in (%configurations%) do (
+	java.exe -cp "C:\Eclipse\configuration\org.eclipse.osgi\bundles\286\1\.cp\lib\scala-library.jar;.\Compiler\bin" Main .\Configs\Sebastian\LSS_%%c_Settings.txt .\Configs\Sebastian\LSS_%%c_Knowledge.txt
+)
