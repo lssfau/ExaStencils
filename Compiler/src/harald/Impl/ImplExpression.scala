f8ba5e1e2ba99cdcd4581412acae670030d403be
kuckuk <sebastian.kuckuk@fau.de>
2014-01-20 10:34:31 +0100

    - simplified ImplVariable

--- Compiler/src/harald/Impl/ImplExpression.scala
+++ Compiler/src/harald/Impl/ImplExpression.scala
@@ -5,11 +5,8 @@ import harald.dsl._
 import harald.ast.TreeManager
 import exastencils.datastructures.ir._
 
-case class ImplVariable(obj : String, n : String, vartype : TypeInfo, lev : Expression, ScopeInfo : String) extends Expression() {
+case class ImplVariable(name : String, vartype : TypeInfo, lev : Expression, ScopeInfo : String) extends Expression() {
   override def duplicate = this.copy().asInstanceOf[this.type]
-
-  val name = n
-  //valu = n
 
   override def cpp : String = {
     var s : String = ""
@@ -18,14 +15,11 @@ case class ImplVariable(obj : String, n : String, vartype : TypeInfo, lev : Expr
         s = vartype.toString_cpp
       else if (ScopeInfo.equals("expression"))
         s = vartype.toString_cpp
-    var objs = obj
-    if (!objs.equals(""))
-      objs = objs + "."
 
     if (lev.cpp /*FIXME: this mechanism is really hard to maintain*/ .equals(""))
-      return objs + name + lev.cpp + s
+      return name + lev.cpp + s
     else
-      return objs + name + "[" + lev.cpp + "]" + s
+      return name + "[" + lev.cpp + "]" + s
   }
 
   def toString_cuda : String = {
@@ -35,24 +29,7 @@ case class ImplVariable(obj : String, n : String, vartype : TypeInfo, lev : Expr
         s = vartype.toString_cuda
       else if (ScopeInfo.equals("expression"))
         s = vartype.toString_cuda
-    var objs = obj
-    if (!objs.equals(""))
-      objs = objs + "."
 
-    return objs + name + s
-  }
-
-  override def evaluate(para : ListBuffer[ParameterInfo]) : Int = {
-
-    // println(para + " " + n)
-    for (p <- para)
-      if (p.name.equals(n))
-        return p.value
-
-    for (c <- DomainKnowledge.global_variables)
-      if (c.name.equals(n))
-        return c.value
-
-    return 0
+    return name + s
   }
 }
