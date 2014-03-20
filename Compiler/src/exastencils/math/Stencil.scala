1325902ecd5dc1564635cf1f3244785e7af61c23
kuckuk <sebastian.kuckuk@fau.de>
2014-03-20 13:22:30 +0100

    - temporarily renamed Hannah's Stencil class to GeneralStencil

--- Compiler/src/exastencils/math/Stencil.scala
+++ Compiler/src/exastencils/math/Stencil.scala
@@ -3,14 +3,14 @@ package exastencils.math
 import scala.language.postfixOps
 
 /** Representation of a general stencil. */
-class Stencil[T](entries : List[ (T, Vector[Int])]) {
+class GeneralStencil[T](entries : List[ (T, Vector[Int])]) {
   assert(entries forall { case (_, v) => v.rows == entries.head._2.rows } )
   
   def withoutCenter() = {
-    new Stencil( entries filter { case (_, off) => !(off isZero) } )
+    new GeneralStencil( entries filter { case (_, off) => !(off isZero) } )
   }
   
   def center() = {
-    new Stencil( entries filter {case (_, off) => off isZero })
+    new GeneralStencil( entries filter {case (_, off) => off isZero })
   }
 }
