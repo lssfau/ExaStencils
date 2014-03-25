10aa25f8fd070a2a32636d25838bd3814eb8cd33
Christian Schmitt <christian.schmitt@cs.fau.de>
2014-03-25 16:55:56 +0100

    Root FieldCollection level comparison

--- Compiler/src/exastencils/datastructures/l4/Root.scala
+++ Compiler/src/exastencils/datastructures/l4/Root.scala
@@ -4,4 +4,8 @@ import exastencils.datastructures._
 
 case class Root(var fields : List[FieldDeclarationStatement], var statements : List[Statement]) extends Node {
   def this(statements : List[Statement]) = this(List(), statements)
+
+  def getFieldByIdentifier(identifier : String, level : Int) : Option[FieldDeclarationStatement] = {
+    fields.find(f => f.name == identifier && f.level.getOrElse(-1) == level)
+  }
 }
