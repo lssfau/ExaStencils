294c7b090c1eb474bbfc6ad6cbe30fe632315597
Christian Schmitt <christian.schmitt@cs.fau.de>
2014-04-25 13:33:58 +0200

    added helper traits for checking of parse tree

--- Compiler/src/exastencils/datastructures/l4/LevelSpecifications.scala
+++ Compiler/src/exastencils/datastructures/l4/LevelSpecifications.scala
@@ -5,22 +5,26 @@ import exastencils.datastructures._
 
 trait LevelSpecification extends Node with Annotatable
 
+// helper traits for checking the parse tree
+trait DeclarationLevelSpecification // can be used for declarations, e.g., functions
+trait AccessLevelSpecification // can be used for accesses, e.g., in fields or function calls
+
 case class SingleLevelSpecification(var level : Int) extends LevelSpecification {
   override def toString() = level.toString
 }
 
-case class RangeLevelSpecification(var begin : Int, var end : Int) extends LevelSpecification
+case class RangeLevelSpecification(var begin : Int, var end : Int) extends LevelSpecification with DeclarationLevelSpecification
 
-case class ListLevelSpecification(var levels : HashSet[LevelSpecification]) extends LevelSpecification {
+case class ListLevelSpecification(var levels : HashSet[LevelSpecification]) extends LevelSpecification with DeclarationLevelSpecification {
   def this() = this(HashSet[LevelSpecification]())
   def this(level : LevelSpecification) = this(HashSet(level))
   def add(level : LevelSpecification) = levels += level
 }
 
-case class CurrentLevelSpecification() extends LevelSpecification
-case class CoarserLevelSpecification() extends LevelSpecification
-case class FinerLevelSpecification() extends LevelSpecification
-case class CoarsestLevelSpecification() extends LevelSpecification
-case class FinestLevelSpecification() extends LevelSpecification
-case class NegatedLevelSpecification(var l : LevelSpecification) extends LevelSpecification
-case class RelativeLevelSpecification(var operator : String, var base : LevelSpecification, var offset : Int) extends LevelSpecification
+case class CurrentLevelSpecification() extends LevelSpecification with DeclarationLevelSpecification with AccessLevelSpecification
+case class CoarserLevelSpecification() extends LevelSpecification with DeclarationLevelSpecification with AccessLevelSpecification
+case class FinerLevelSpecification() extends LevelSpecification with DeclarationLevelSpecification with AccessLevelSpecification
+case class CoarsestLevelSpecification() extends LevelSpecification with DeclarationLevelSpecification with AccessLevelSpecification
+case class FinestLevelSpecification() extends LevelSpecification with DeclarationLevelSpecification with AccessLevelSpecification
+case class NegatedLevelSpecification(var l : LevelSpecification) extends LevelSpecification with DeclarationLevelSpecification
+case class RelativeLevelSpecification(var operator : String, var base : LevelSpecification, var offset : Int) extends LevelSpecification with DeclarationLevelSpecification with AccessLevelSpecification
