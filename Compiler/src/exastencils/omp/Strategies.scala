5290bc1e712b6e1511e0d7f3a57bf5f2c2fda34f
Sebastian Kuckuk <sebastian.kuckuk@fau.de>
2014-05-20 09:33:01 +0200

    - moved AddOMPPragmas strategy to appropriate file

---
+++ Compiler/src/exastencils/omp/Strategies.scala
@@ -0,0 +1,21 @@
+package exastencils.omp
+
+import exastencils.knowledge._
+import exastencils.datastructures._
+import exastencils.datastructures.ir._
+
+object AddOMPPragmas extends DefaultStrategy("Adding OMP pragmas") {
+  if (Knowledge.omp_requiresCriticalSections)
+    this += new Transformation("Adding OMP critical pragmas", {
+      case target : OMP_PotentiallyCritical => target match {
+        case target : Scope     => new OMP_Critical(target)
+        case target : Statement => new OMP_Critical(target)
+      }
+    }, false)
+
+  this += new Transformation("Adding OMP parallel for pragmas", {
+    case target : ForLoopStatement with OMP_PotentiallyParallel =>
+      new OMP_ParallelFor(new ForLoopStatement(target.begin, target.end, target.inc, target.body, target.reduction),
+        (if (target.reduction.isDefined) target.reduction.get.getOMPClause else new NullExpression), target.collapse)
+  })
+}
--- Compiler/src/exastencils/strategies/Strategies.scala
+++ Compiler/src/exastencils/strategies/Strategies.scala
@@ -159,19 +159,3 @@ object AddMemberFunctionPrefix extends DefaultStrategy("Adding member function p
       c
   })
 }
-
-object AddOMPPragmas extends DefaultStrategy("Adding OMP pragmas") {
-  if (Knowledge.omp_requiresCriticalSections)
-    this += new Transformation("Adding OMP critical pragmas", {
-      case target : OMP_PotentiallyCritical => target match {
-        case target : Scope     => new OMP_Critical(target)
-        case target : Statement => new OMP_Critical(target)
-      }
-    }, false)
-
-  this += new Transformation("Adding OMP parallel for pragmas", {
-    case target : ForLoopStatement with OMP_PotentiallyParallel =>
-      new OMP_ParallelFor(new ForLoopStatement(target.begin, target.end, target.inc, target.body, target.reduction),
-        (if (target.reduction.isDefined) target.reduction.get.getOMPClause else new NullExpression), target.collapse)
-  })
-}
