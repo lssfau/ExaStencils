9ba4682d114188ce616bb45f5d6750e357ee739a
Sebastian Kuckuk <sebastian.kuckuk@fau.de>
2014-05-21 10:20:58 +0200

    - cleaned up Main
    NOTE: THIS VERSION SHOULD GENERATE VALID CODE AGAIN

--- Compiler/src/Main.scala
+++ Compiler/src/Main.scala
@@ -1,28 +1,21 @@
 import scala.collection.mutable.ListBuffer
 import exastencils.core._
 import exastencils.knowledge._
+import exastencils.parsers.l4._
+import exastencils.languageprocessing.l4._
 import exastencils.datastructures._
-import exastencils.datastructures.ir._
 import exastencils.datastructures.ir.ImplicitConversions._
+import exastencils.prettyprinting._
+import exastencils.globals._
 import exastencils.strategies._
 import exastencils.application._
 import exastencils.domain._
 import exastencils.multiGrid._
 import exastencils.primitives._
 import exastencils.util._
-import exastencils.globals._
-import exastencils.prettyprinting.PrettyprintingManager
-import harald_dep.Parser._
-import harald_dep.dsl._
-import harald_dep.Generate._
-import harald_dep.ast._
-import exastencils.spl.FeatureModel
-import exastencils.parsers.l4.ParserL4
-import exastencils.parsers.l4.ValidationL4
-import exastencils.datastructures.l4.ProgressableToIr
-import exastencils.languageprocessing.l4.ProgressToIr
 import exastencils.mpi._
 import exastencils.omp._
+import exastencils.spl.FeatureModel
 import exastencils.polyhedron._
 
 object Main {
@@ -40,27 +33,16 @@ object Main {
 
     Knowledge.update()
 
-    // feature model  (modified FAMA Format (http://www.isa.us.es/fama/))
-    //    FeatureModel.readFeatureModel(Settings.basePathPrefix + "/Compiler/featureModel/model_Prototype.model")
-    //    var configuration = FeatureModel.getMinimalConfig
-    //    Knowledge.update(configuration)
-
-    // Hack paths (relative paths should work here, too, if not, reverse this change)
-    // ... this obviously depends on the execution path which in my case is the root folder to include configs and scripts
-    val DSLpath = Settings.basePathPrefix + "/Compiler/src/harald_dep/testmg/"
-    val problem = "testDSL"
-    val outputfile = "main.cpp"
-
     // HACK: this tests the new L4 capabilities
     var parserl4 = new ParserL4
     StateManager.root_ = parserl4.parseFile(Settings.basePathPrefix + "/Compiler/dsl/newDSL4.exa")
     ValidationL4.apply
     ProgressToIr.apply()
 
-    StateManager.root_ = StateManager.root_.asInstanceOf[ProgressableToIr].progressToIr.asInstanceOf[Node]
+    StateManager.root_ = StateManager.root_.asInstanceOf[l4.ProgressableToIr].progressToIr.asInstanceOf[Node]
 
     // Setup tree
-    StateManager.root_.asInstanceOf[Root].nodes ++= List(
+    StateManager.root_.asInstanceOf[ir.Root].nodes ++= List(
       // Application
       new Poisson3D,
 
@@ -78,91 +60,6 @@ object Main {
 
       // Globals
       new Globals)
-
-    // FIXME: set fields in L4
-    /*var fieldCollection = StateManager.findFirst[FieldCollection]().get
-    for (level <- 0 to Knowledge.maxLevel) {
-      { // fields requiring (ghost-layer) communication
-        val layout = DimArray().map(dim => new FieldLayoutPerDim(if (0 == dim) 1 else 0, Knowledge.data_numGhostLayers, 1, ((Knowledge.domain_fragLengthPerDim(dim) * (1 << level)) + 1) - 2 /*dup*/ , 1, Knowledge.data_numGhostLayers, 0)) ++
-          (Knowledge.dimensionality until 3).toArray.map(dim => new FieldLayoutPerDim(0, 0, 0, 1, 0, 0, 0))
-        fieldCollection.fields += new Field("Solution", 0, "solData", "double", layout, level, Knowledge.data_numSolSlots, new MultiIndex(layout.map(l => l.idxDupLeftBegin)), true)
-        fieldCollection.fields += new Field("Residual", 0, "resData", "double", layout, level, Knowledge.data_numSolSlots, new MultiIndex(layout.map(l => l.idxDupLeftBegin)), false)
-      }
-      { // fields without ghost layers
-        val layout = DimArray().map(dim => new FieldLayoutPerDim(0, 0, 1, ((Knowledge.domain_fragLengthPerDim(dim) * (1 << level)) + 1) - 2 /*dup*/ , 1, 0, 0)) ++
-          (Knowledge.dimensionality until 3).toArray.map(dim => new FieldLayoutPerDim(0, 0, 0, 1, 0, 0, 0))
-        fieldCollection.fields += new Field("RHS", 0, "rhsData", "double", layout, level, Knowledge.data_numSolSlots, new MultiIndex(layout.map(l => l.idxDupLeftBegin)), false)
-      }
-    }*/
-
-    // setup basic sub-nodes
-
-    //do { ExpandStrategy.apply }
-    //while (ExpandStrategy.results.last._2.replacements > 0) // FIXME: cleaner code
-
-    // Harald
-
-    println("read HW")
-
-    if (!new java.io.File(DSLpath + problem + "levHW.mg").exists) {
-      println("HW specification is missing!")
-      sys.exit(0)
-    }
-
-    val DSLHW : String = scala.io.Source.fromFile(DSLpath + problem + "levHW.mg").getLines.reduceLeft(_ + '\n' + _)
-    //println(DSLHW)
-
-    val parserHW = new ParserHW
-    parserHW.parseAll(parserHW.exastencilsHW, DSLHW)
-
-    harald_dep.dsl /*FIXME*/ .Hardware.initHWFeatures
-
-    if (!new java.io.File(DSLpath + problem + "lev1.mg").exists) {
-      println("Problem specification (DSL level 1) is missing!")
-      sys.exit(0)
-    }
-
-    println("read PDE")
-    val DSLl1 : String = scala.io.Source.fromFile(DSLpath + problem + "lev1.mg").getLines.reduceLeft(_ + '\n' + _)
-    //println(DSLl1)
-
-    val parserl1 = new ParserL1
-    parserl1.parseAll(parserl1.exastencilsL1, DSLl1)
-
-    if (!new java.io.File(DSLpath + problem + "lev2.mg").exists || DomainKnowledge.generate_L1.getOrElse(1) == 1)
-      GenerateL2.transformL1toL2(DSLpath + problem + "lev2.mg")
-
-    println("read discretization")
-    val DSLl2 : String = scala.io.Source.fromFile(DSLpath + problem + "lev2.mg").getLines.reduceLeft(_ + _)
-    //println(DSLl2)
-
-    val parserl2 = new ParserL2(TreeManager.tree)
-    parserl2.parseAll(parserl2.exastencilsL2, DSLl2)
-    TreeManager.tree.exaFields.foreach(println)
-    TreeManager.tree.exaOperators.foreach(println)
-    DomainKnowledge.initfragments
-
-    val genL3 = new GenerateL3(TreeManager.tree)
-    if (!new java.io.File(DSLpath + problem + "lev3.mg").exists || DomainKnowledge.generate_L1.getOrElse(1) == 1)
-      genL3.transformL2toL3(DSLpath + problem + "lev3.mg")
-
-    val DSLl3 : String = scala.io.Source.fromFile(DSLpath + problem + "lev3.mg").getLines.reduceLeft(_ + _)
-    //println(DSLl3)
-
-    val parserl3 = new ParserL3
-    parserl3.parseAll(parserl3.exastencilsL3, DSLl3)
-    DomainKnowledge.initarraysizes
-
-    val genL4 = new GenerateL4(TreeManager.tree)
-    if (!(new java.io.File(DSLpath + problem + "lev4.mg").exists) || (DomainKnowledge.generate_L1.getOrElse(1) == 1)) {
-      genL4.transformL3toL4(DSLpath + problem + "lev4.mg")
-      println("generate L4: " + DomainKnowledge.generate_L1.getOrElse(1))
-    }
-    val DSLl4 : String = scala.io.Source.fromFile(DSLpath + problem + "lev4.mg").getLines.reduceLeft(_ + _)
-    //println(DSLl4)
-
-    //    val parserl4_dep = new harald_dep.Parser.ParserL4(TreeManager.tree)
-    //    parserl4_dep.parse(DSLl4)
 
     // Strategies
 
