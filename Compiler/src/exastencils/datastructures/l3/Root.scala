06d3424b64b8d247e886d3d6c310f012ec6c6ea4
kuckuk <sebastian.kuckuk@fau.de>
2014-05-30 12:51:08 +0200

    - fixed generation of external field specifications on l4

--- Compiler/src/exastencils/datastructures/l3/Root.scala
+++ Compiler/src/exastencils/datastructures/l3/Root.scala
@@ -11,7 +11,7 @@ case class Root() extends Node {
   var numPre : Int = 2 // has to be divisible by 2 for Jac
   var numPost : Int = 4 // has to be divisible by 2 for Jac
   var testBC : Boolean = true
-  var testExtFields : Boolean = false
+  var testExtFields : Boolean = true
   var printFieldAtEnd : Boolean = false
 
   def printToL4(filename : String) : Unit = {
@@ -31,29 +31,43 @@ case class Root() extends Node {
     // Layouts
     Knowledge.dimensionality match {
       case 2 => {
-        printer.println("""Layout BasicComm {
-	ghostLayers = [ 1, 1 ] with communication
-	duplicateLayers = [ 1, 1 ] with communication
-	// innerPoints = []
-}""")
-        printer.println("""Layout NoComm {
-	ghostLayers = [ 0, 0 ]
-	duplicateLayers = [ 1, 1 ]
-	// innerPoints = []
-}""")
+        printer.println("Layout BasicComm {")
+        printer.println("\tghostLayers = [ 1, 1 ] with communication")
+        printer.println("\tduplicateLayers = [ 1, 1 ] with communication")
+        printer.println("\t// innerPoints = []")
+        printer.println("}")
+        printer.println("Layout NoComm {")
+        printer.println("\tghostLayers = [ 0, 0 ]")
+        printer.println("\tduplicateLayers = [ 1, 1 ]")
+        printer.println("\t// innerPoints = []")
+        printer.println("}")
+        if (testExtFields) {
+          printer.println("Layout ExtSolLayout {")
+          printer.println("\tghostLayers = [ 0, 0 ]")
+          printer.println("\tduplicateLayers = [ 0, 0 ]")
+          printer.println(s"\tinnerPoints = [ ${(Knowledge.domain_fragLengthPerDim(0) * (1 << Knowledge.maxLevel)) + 1}, ${(Knowledge.domain_fragLengthPerDim(1) * (1 << Knowledge.maxLevel)) + 1} ]")
+          printer.println("}")
+        }
       }
 
       case 3 => {
-        printer.println("""Layout BasicComm {
-	ghostLayers = [ 1, 1, 1 ] with communication
-	duplicateLayers = [ 1, 1, 1 ] with communication
-	// innerPoints = []
-}""")
-        printer.println("""Layout NoComm {
-	ghostLayers = [ 0, 0, 0 ]
-	duplicateLayers = [ 1, 1, 1 ]
-	// innerPoints = []
-}""")
+        printer.println("Layout BasicComm {")
+        printer.println("\tghostLayers = [ 1, 1, 1 ] with communication")
+        printer.println("\tduplicateLayers = [ 1, 1, 1 ] with communication")
+        printer.println("\t// innerPoints = []")
+        printer.println("}")
+        printer.println("Layout NoComm {")
+        printer.println("\tghostLayers = [ 0, 0, 0 ]")
+        printer.println("\tduplicateLayers = [ 1, 1, 1 ]")
+        printer.println("\t// innerPoints = []")
+        printer.println("}")
+        if (testExtFields) {
+          printer.println("Layout ExtSolLayout {")
+          printer.println("\tghostLayers = [ 0, 0, 0 ]")
+          printer.println("\tduplicateLayers = [ 0, 0, 0 ]")
+          printer.println(s"\tinnerPoints = [ ${(Knowledge.domain_fragLengthPerDim(0) * (1 << Knowledge.maxLevel)) + 1}, ${(Knowledge.domain_fragLengthPerDim(1) * (1 << Knowledge.maxLevel)) + 1}, ${(Knowledge.domain_fragLengthPerDim(2) * (1 << Knowledge.maxLevel)) + 1} ]")
+          printer.println("}")
+        }
       }
     }
     printer.println
@@ -81,7 +95,7 @@ case class Root() extends Node {
 
     // External Fields
     if (testExtFields) {
-      printer.println("external Field extSolution <bla> => Solution @(finest)")
+      printer.println("external Field extSolution <ExtSolLayout> => Solution@(finest)")
       printer.println
     }
 
