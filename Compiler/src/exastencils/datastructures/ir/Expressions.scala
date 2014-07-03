4ea31bb38f2ac68852653e47e3cf0d1b621d9691
Stefan Kronawitter <stefan.kronawitter@uni-passau.de>
2014-07-03 09:45:48 +0200

    fix/workaround (for my eclipse installation)

--- Compiler/src/exastencils/datastructures/ir/Expressions.scala
+++ Compiler/src/exastencils/datastructures/ir/Expressions.scala
@@ -268,7 +268,7 @@ case class LinearizedFieldAccess(var fieldSelection : FieldSelection, var index 
   override def cpp : String = "NOT VALID ; CLASS = ExternalFieldAccess\n"
 
   override def expand : Expression = {
-    new ArrayAccess(iv.FieldData(fieldSelection.field, fieldSelection.slot, fieldSelection.fragIdx), index.cpp)
+    new ArrayAccess(new iv.FieldData(fieldSelection.field, fieldSelection.slot, fieldSelection.fragIdx), index.cpp)
   }
 }
 
