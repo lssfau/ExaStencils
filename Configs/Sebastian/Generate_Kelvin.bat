362fb93d6f8b40e365c4b2ebb67d8cdd85c3e1ca
kuckuk <sebastian.kuckuk@fau.de>
2014-08-08 09:29:53 +0200

    - updated my local config and added one for Kelvin's project

---
+++ Configs/Sebastian/Generate_Kelvin.bat
@@ -0,0 +1,2 @@
+java.exe -cp "C:\Eclipse\plugins\org.scala-lang.scala-library_2.11.2.v20140721-095018-73fb460c1c.jar;C:\Eclipse\plugins\org.scala-lang.scala-reflect_2.11.2.v20140721-095018-73fb460c1c.jar;.\Compiler\bin;.\CompilerMacros\CompilerMacros\bin;.\Compiler\lib\*" Main .\Configs\Sebastian\Kelvin_Settings.txt .\Configs\Sebastian\Kelvin_Knowledge.txt
+
