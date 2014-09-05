ca877e110f7d22b1703251cb54256e4c77bb540e
Kronawitter Stefan <kronast@fim.uni-passau.de>
2014-09-05 12:08:48 +0200

    hack to enable usage of isl options (which are stored in the context) until this functionality is added to the bindings

--- Compiler/src/exastencils/polyhedron/PolyOpt.scala
+++ Compiler/src/exastencils/polyhedron/PolyOpt.scala
@@ -33,8 +33,8 @@ object PolyOpt extends CustomStrategy("Polyhedral optimizations") {
     this.transaction()
     Logger.info("Applying strategy " + name)
 
-//    isl.Options.setTileScaleTileLoops(false)
-//    isl.Options.setTileShiftPointLoops(false)
+    isl.Options.setTileScaleTileLoops(false)
+    isl.Options.setTileShiftPointLoops(false)
 
     val scops : Seq[Scop] = extractPolyModel()
     for (scop <- scops if (!scop.remove)) {
