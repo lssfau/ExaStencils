0a98c9eb0daa54f6bab6d801dc432e6f808425f6
Christian Schmitt <christian.schmitt@cs.fau.de>
2014-09-17 13:02:11 +0200

    Comments

--- Compiler/src/exastencils/core/UniversalSetter.scala
+++ Compiler/src/exastencils/core/UniversalSetter.scala
@@ -2,7 +2,18 @@ package exastencils.core
 
 import reflect.runtime.universe._
 
+/**
+  * A utility object to set values of any entity by just specifying the instance, the name of the member and the value.
+  *
+  */
 object UniversalSetter {
+  /**
+    * Sets the value of an entity's member.
+    *
+    * @param obj The instance of the entity whose member is to be set.
+    * @param ident The name of the member to be set.
+    * @param value The new value of the member.
+    */
   def apply[T](obj : AnyRef, ident : String, value : T) : Unit = {
     Logger.info("UniversalSetter: Setting " + ident + " to " + value)
 
@@ -23,6 +34,15 @@ object UniversalSetter {
     field.setAccessible(accessible)
   }
 
+  /**
+    * Converts the new value into the matching type and sets the value of an entity's member.
+    *
+    * This methods converts the value into a suitable type (e.g., by parsing a string into Int or Float) before setting the new value.
+    *
+    * @param obj The instance of the entity whose member is to be set.
+    * @param ident The name of the member to be set.
+    * @param value The new value of the member.
+    */
   def withConversion[T : TypeTag](obj : AnyRef, ident : String, value : T) : Unit = {
     Logger.info("UniversalSetter: Setting (with conversion) " + ident + " to " + value)
 
