26f004150364ca71dd4753de7f254d63d02b2972
Christian Schmitt <christian.schmitt@cs.fau.de>
2014-09-17 13:21:04 +0200

    Comments

--- Compiler/src/exastencils/datastructures/Transformation.scala
+++ Compiler/src/exastencils/datastructures/Transformation.scala
@@ -5,16 +5,30 @@ import scala.collection.GenTraversableOnce
 import scala.collection.mutable.ListBuffer
 import scala.language.higherKinds
 
-// FIXME extend this to PartialFunction[Any(Ref), Transformation.Output[_]]
+/**
+  * The class to specify a transformation modifying a program state into a new one.
+  *
+  * @param name The name of the transformation. Used for debugging purposes.
+  * @param function A Scala function specifying the nodes to look for and specifying their replacement.
+  * @param recursive Specifies if the Transformation is to be applied to subnodes of a node that has just been replaced.
+  * @param applyAtNode Specifies the source node where the transformation starts to traverse the program state.
+  */
 class Transformation(val name : String, val function : PartialFunction[Node, Transformation.OutputType], val recursive : Boolean = true, val applyAtNode : Option[Node] = None) {
   override def toString() = s"""Transformation "$name""""
 }
 
+/**
+  * Companion object for the Transformation class.
+  */
 object Transformation {
   import scala.language.implicitConversions
 
   // workaround since Scala's type system does not allow for real union types
   type Or[A, B] = Either[A, B]
+
+  /**
+    * The result type for Transformation functions.
+    */
   class Output[T <: AnyRef](val inner : T)(implicit val ev : T => Node Or NodeList Or Option[Nothing])
 
   // implicit defs for workaround
@@ -35,15 +49,32 @@ object Transformation {
 
   type OutputType = Output[_ <: AnyRef]
 
+  /**
+    * Companion object for the Transformation.Output class.
+    */
   object Output {
     def apply[T <: AnyRef](inner : T)(implicit ev : T => Node Or NodeList Or Option[Nothing]) = new Output(inner)
   }
 
+  /**
+    * Creates a new Transformation which, for example, can be added to a Strategy.
+    *
+    * @param name The name of the transformation. Used for debugging purposes.
+    * @param function A Scala function specifying the nodes to look for and specifying their replacement.
+    * @param recursive Specifies if the Transformation is to be applied to subnodes of a node that has just been replaced.
+    * @param applyAtNode Specifies the source node where the transformation starts to traverse the program state.
+    */
   def apply(name : String, function : PartialFunction[Node, OutputType], recursive : Boolean = true, applyAtNode : Option[Node] = None) = {
     new Transformation(name, function, recursive, applyAtNode)
   }
 }
 
+/**
+ * Represents some statistics about a Transformation.
+ * 
+ * @param successful Specifies if the Transformation did finish successfully (independent of the number of matches) or was aborted.
+ * @param matches The number of matches - and nodes replaced - by the Transformation.
+ */
 class TransformationResult(val successful : Boolean, val matches : Int) {
   override def toString() = {
     var s = "Transformation Result: "
