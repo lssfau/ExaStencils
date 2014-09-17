9480770436c41a975cfe11a8191de1b51fabd0da
Christian Schmitt <christian.schmitt@cs.fau.de>
2014-09-17 10:32:49 +0200

    Added Scaladoc comments to Logger

--- CompilerMacros/CompilerMacros/src/exastencils/core/Logger.scala
+++ CompilerMacros/CompilerMacros/src/exastencils/core/Logger.scala
@@ -1,7 +1,11 @@
 package exastencils.core
 
-import scala.reflect.macros.blackbox
-
+/**
+  * Logger instance that currently writes to stdout.
+  *
+  * Because it uses Scala macros that need to be defined in a separate compilation run, this is a separate project.
+  *
+  */
 object Logger {
   import scala.reflect.macros._
   import scala.reflect.runtime.universe._
@@ -11,19 +15,77 @@ object Logger {
   // Level 2: Debug
   // Level 4: Info
 
+  /** The level for displaying only errors. */
+  val LEVEL_ERROR = 0
+
+  /** The level for displaying errors and warnings. */
+  val LEVEL_WARNING = 1
+
+  /** The level for displaying errors, warnings and debug messages. */
+  val LEVEL_DEBUG = 2
+
+  /** The level for displaying errors, warnings, debug messages and info messages. */
+  val LEVEL_INFO = 4
+
   protected var current : Int = 2
+
+  /**
+    * Returns the current logging level.
+    *
+    * @return The current logging level.
+    */
   def getLevel : Int = current
+
+  /**
+    * Sets the current logging level.
+    *
+    * @param level A numerical value denoting the logging level, i.e., one of the LEVEL_* values defined in this entity.
+    */
   def setLevel(level : Int) = { current = level }
 
+  /**
+    * Outputs an error message and exits the program.
+    *
+    * @param s The message to display.
+    */
   def error(s : AnyRef) = {
     sys.error("ERROR: " + s)
     sys.exit(-1) // just to be extra sure
   }
 
+  /**
+    * Outputs a warning message.
+    *
+    * @param s The message to display.
+    */
   def warn(s : AnyRef) : Unit = macro warnImpl
+
+  /**
+    * Outputs a warning message.
+    *
+    * @param s The message to display.
+    */
   def warning(s : AnyRef) : Unit = macro warnImpl
+
+  /**
+    * Outputs a debug message.
+    *
+    * @param s The message to display.
+    */
   def debug(s : AnyRef) : Unit = macro dbgImpl
+
+  /**
+    * Outputs a debug message.
+    *
+    * @param s The message to display.
+    */
   def dbg(s : AnyRef) : Unit = macro dbgImpl
+
+  /**
+    * Outputs a info message.
+    *
+    * @param s The message to display.
+    */
   def info(s : AnyRef) : Unit = macro infoImpl
 
   def warnImpl(c : blackbox.Context)(s : c.Expr[AnyRef]) : c.Expr[Unit] = {
