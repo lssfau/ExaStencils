84476edf2251564ad02bf31b6b8a6e68f2cee6c3
Christian Schmitt <christian.schmitt@cs.fau.de>
2014-09-19 08:17:47 +0200

    rename identifiers having the same name as C++ keywords protect internal identifiers

--- Compiler/src/exastencils/parsers/l4/ValidationL4.scala
+++ Compiler/src/exastencils/parsers/l4/ValidationL4.scala
@@ -7,9 +7,7 @@ import exastencils.datastructures.l4._
 object ValidationL4 {
   val s = DefaultStrategy("Validate L4 Input")
 
-  s += Transformation("Check identifiers names", {
-    case x : Identifier if (x.name.startsWith("_")) => Logger.error(s"""Illegal identifier "${x.name}": Identifiers may not start with '_'""")
-  })
-
+  // FIXME
+  
   def apply() = s.apply()
 }
