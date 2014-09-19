9290094c0e6174277809c7d11f96f2c36c4db46a
Christian Schmitt <christian.schmitt@cs.fau.de>
2014-09-19 08:13:20 +0200

    Added comments

--- Compiler/src/exastencils/core/Exceptions.scala
+++ Compiler/src/exastencils/core/Exceptions.scala
@@ -3,7 +3,25 @@ package exastencils.core
 import exastencils.datastructures.Strategy
 import exastencils.datastructures.Transformation
 
+/**
+ * Exception that is thrown in the context of [[exastencils.datastructures.Transformation]].
+ * 
+ * @param msg A message describing the error.
+ * @param transformation The [[exastencils.datastructures.Transformation]] causing the exception.
+ */
 case class TransformationException(msg : String, transformation : Transformation) extends RuntimeException(msg)
 
+/**
+ * Exception that is thrown in the context of Transactions.
+ * 
+ * @param msg A message describing the error.
+ */
 case class TransactionException(msg : String) extends RuntimeException(msg)
+
+/**
+ * Exception that is thrown in the context of Checkpoints.
+ * 
+ * @param msg A message describing the error.
+ * @param inner The inner exception leading to the CheckpointException, or None.
+ */
 case class CheckpointException(msg : String, inner : Option[Throwable] = None) extends RuntimeException(msg)
