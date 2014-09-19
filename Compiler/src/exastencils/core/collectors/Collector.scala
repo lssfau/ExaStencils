5533f85524940a80ac95ee2cedfc60c24ff643cf
Christian Schmitt <christian.schmitt@cs.fau.de>
2014-09-19 17:07:50 +0200

    Added comments

--- Compiler/src/exastencils/core/collectors/Collector.scala
+++ Compiler/src/exastencils/core/collectors/Collector.scala
@@ -2,8 +2,34 @@ package exastencils.core.collectors
 
 import exastencils.datastructures._
 
+/**
+  * Keeps track of [[exastencils.datastructures.Node]]s that are traversed during the application of [[exastencils.datastructures.Transformation]]s.
+  *
+  * Collectors follow the Observer-pattern. They are added to the [[exastencils.core.StateManager]], who will notify
+  * the observers whenever a [[exastencils.datastructures.Node]] is entered or left during the traversal of the program
+  * state. This is a basic trait that must be implemented by concrete implementations.
+  */
 abstract trait Collector {
+  /**
+    * Is called when a new [[exastencils.datastructures.Node]] is visited.
+    *
+    * @param node The [[exastencils.datastructures.Node]] currently visited.
+    */
   def enter(node : Node) : Unit
+
+  /**
+    * Is called when traversal of a [[exastencils.datastructures.Node]] is done and it is left.
+    *
+    * @param node The [[exastencils.datastructures.Node]] just left.
+    */
   def leave(node : Node) : Unit
+
+  /**
+    * Resets the internal state of the Collector.
+    *
+    * This method resets the internal state of the Collector.
+    * [[exastencils.core.StateManager]] calls this method before applying a new [[exastencils.datastructures.Transformation]].
+    * This may be used to clear the history of a Collector that keeps a protocol of the visited [[exastencils.datastructures.Node]]s.
+    */
   def reset() : Unit
 }
