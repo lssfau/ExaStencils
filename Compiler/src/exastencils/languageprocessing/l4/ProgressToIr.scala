30d92f7ee50bd6489071a6efed98350179a8ba5f
Christian Schmitt <christian.schmitt@cs.fau.de>
2014-09-19 17:01:00 +0200

    temporary fix for return statements

--- Compiler/src/exastencils/languageprocessing/l4/ProgressToIr.scala
+++ Compiler/src/exastencils/languageprocessing/l4/ProgressToIr.scala
@@ -52,7 +52,7 @@ object ProgressToIr extends DefaultStrategy("ProgressToIr") {
     "const", "constexpr", "const_cast", "continue", "decltype", "default", "delete", "do", "double", "dynamic_cast",
     "else", "enum", "explicit", "export", "extern", "false", "float", "for", "friend", "goto", "if", "inline", "int",
     "long", "mutable", "namespace", "new", "noexcept", "not", "not_eq", "nullptr", "operator", "or", "or_eq", "private",
-    "protected", "public", "register", "reinterpret_cast", "return", "short", "signed", "sizeof", "static", "static_assert",
+    "protected", "public", "register", "reinterpret_cast", /*"return",*/ "short", "signed", "sizeof", "static", "static_assert",
     "static_cast", "struct", "switch", "template", "this", "thread_local", "throw", "true", "try", "typedef",
     "typeid", "typename", "union", "unsigned", "using", "virtual", "void", "volatile", "wchar_t", "while", "xor", "xor_eq")
 
