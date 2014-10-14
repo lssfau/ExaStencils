624923c6c88c3e9a148c905d07b7b8eafa5fd7bc
kuckuk <sebastian.kuckuk@fau.de>
2014-10-14 16:02:39 +0200

    - updated l3 generation routines to reflect the changes in field and field layout declaration

--- Compiler/src/exastencils/datastructures/l3/Generate/Layouts.scala
+++ Compiler/src/exastencils/datastructures/l3/Generate/Layouts.scala
@@ -6,12 +6,21 @@ object Layouts {
   def genSet(mapping : (Int => Int)) = { (0 until Knowledge.dimensionality).toArray.map(mapping).mkString(", ") }
 
   def addLayouts(printer : java.io.PrintWriter) = {
-    printer.println(s"Layout NoComm {")
+    var fieldDatatype = (if (Knowledge.l3tmp_genVectorFields) s"Array[Real][${Knowledge.l3tmp_numVecDims}]" else "Real")
+
+    printer.println(s"Layout NoComm< $fieldDatatype > {")
     printer.println(s"\tghostLayers = [ ${genSet(_ => 0)} ]")
     printer.println(s"\tduplicateLayers = [ ${genSet(_ => 1)} ]")
     printer.println(s"}")
 
-    printer.println(s"Layout CommPartTempBlockable {")
+    if (Knowledge.l3tmp_genVectorFields) {
+      printer.println(s"Layout NoCommScalar< Real > {") // TODO: @coarsest
+      printer.println(s"\tghostLayers = [ ${genSet(_ => 0)} ]")
+      printer.println(s"\tduplicateLayers = [ ${genSet(_ => 1)} ]")
+      printer.println(s"}")
+    }
+
+    printer.println(s"Layout CommPartTempBlockable< $fieldDatatype > {")
     if (Knowledge.l3tmp_genTemporalBlocking) {
       printer.println(s"\tghostLayers = [ ${genSet(_ => Knowledge.l3tmp_numPre - 1)} ] with communication")
       printer.println(s"\tduplicateLayers = [ ${genSet(_ => 1)} ] with communication")
@@ -21,12 +30,19 @@ object Layouts {
     }
     printer.println(s"}")
 
-    printer.println(s"Layout BasicComm {")
+    printer.println(s"Layout BasicComm< $fieldDatatype > {")
     printer.println(s"\tghostLayers = [ ${genSet(_ => 1)} ] with communication")
     printer.println(s"\tduplicateLayers = [ ${genSet(_ => 1)} ] with communication")
     printer.println(s"}")
 
-    printer.println(s"Layout CommFullTempBlockable {")
+    if (Knowledge.l3tmp_genVectorFields) {
+      printer.println(s"Layout BasicCommScalar< Real > {") // TODO: @coarsest
+      printer.println(s"\tghostLayers = [ ${genSet(_ => 1)} ] with communication")
+      printer.println(s"\tduplicateLayers = [ ${genSet(_ => 1)} ] with communication")
+      printer.println(s"}")
+    }
+
+    printer.println(s"Layout CommFullTempBlockable< $fieldDatatype > {")
     if (Knowledge.l3tmp_genTemporalBlocking)
       printer.println(s"\tghostLayers = [ ${genSet(_ => Knowledge.l3tmp_numPre)} ] with communication")
     else
@@ -35,7 +51,7 @@ object Layouts {
     printer.println(s"}")
 
     if (Knowledge.l3tmp_genExtFields) {
-      printer.println(s"Layout ExtSolLayout {")
+      printer.println(s"Layout ExtSolLayout< $fieldDatatype > {")
       printer.println(s"\tghostLayers = [ ${genSet(_ => 0)} ]")
       printer.println(s"\tduplicateLayers = [ ${genSet(_ => 0)} ]")
       printer.println(s"\tinnerPoints = [ ${genSet(i => (Knowledge.domain_fragLengthPerDim(i) * (1 << Knowledge.maxLevel)) + 1)} ]")
