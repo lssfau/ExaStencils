e50c5b8ad045c668d27043b8c8cb40df2c539654
Kronawitter Stefan <kronast@fim.uni-passau.de>
2014-11-04 13:11:20 +0100

    rework Unrolling strategy to allow reusing parts in later specialized unroller (e.g. for correction code, which is work in progress)

--- Compiler/src/exastencils/optimization/Unrolling.scala
+++ Compiler/src/exastencils/optimization/Unrolling.scala
@@ -4,7 +4,6 @@ import scala.collection.mutable.HashSet
 import scala.collection.mutable.ListBuffer
 
 import exastencils.core._
-import exastencils.core.collectors.Collector
 import exastencils.datastructures._
 import exastencils.datastructures.ir._
 import exastencils.knowledge.Knowledge
@@ -13,7 +12,7 @@ import exastencils.util.SimplifyExpression
 
 object Unrolling extends DefaultStrategy("Loop unrolling") {
 
-  final val NO_REM_ANNOT : String = "UnrNR"
+  final val NO_REM_ANNOT : String = "URNR"
 
   this += new Transformation("optimize", UnrollInnermost)
 }
@@ -23,8 +22,7 @@ private final case class UnrollException(msg : String) extends Exception(msg)
 private final object UnrollInnermost extends PartialFunction[Node, Transformation.OutputType] {
 
   private final val DEBUG : Boolean = false
-
-  private final val SKIP_ANNOT : String = "UnRSkip"
+  private final val SKIP_ANNOT : String = "URSkip"
 
   def isDefinedAt(node : Node) : Boolean = {
     return node match {
@@ -40,6 +38,8 @@ private final object UnrollInnermost extends PartialFunction[Node, Transformatio
     val loop = node.asInstanceOf[ForLoopStatement with OptimizationHint]
     val annot : Option[Annotation] = node.removeAnnotation(Unrolling.NO_REM_ANNOT)
     val oldOffset : Long = if (annot.isDefined) annot.get.value.asInstanceOf[Long] else 0L
+    if (oldOffset < 0)
+      return node
     var njuOffset : Long = 0
 
     var njuBegin : VariableDeclarationStatement = null
@@ -49,7 +49,7 @@ private final object UnrollInnermost extends PartialFunction[Node, Transformatio
     var oldInc : Long = 0
     try {
       // do not modify original structure, as an exception could be thrown, which requires a rollback
-      val res : (Statement, String, Long) = updateIncrement(loop.inc)
+      val res : (Statement, String, Long) = updateIncrement(loop.inc, Knowledge.opt_unroll)
       njuIncr = res._1
       itVar = res._2
       oldInc = res._3
@@ -65,12 +65,14 @@ private final object UnrollInnermost extends PartialFunction[Node, Transformatio
 
     val res = new ListBuffer[Statement]()
     if (njuBegin != null) {
-      if (loop.isInstanceOf[OMP_PotentiallyParallel]) { // allow omp parallelization
-        loop.asInstanceOf[OMP_PotentiallyParallel].addOMPStatements += "lastprivate(" + itVar + ')'
-        loop.begin = AssignmentStatement(VariableAccess(itVar, Some(IntegerDatatype())), njuBegin.expression.get, "=")
-        njuBegin.expression = None
-      } else
-        loop.begin = NullStatement // only remove if we add a new declaration before
+      loop match {
+        case parLoop : ForLoopStatement with OMP_PotentiallyParallel => // allow omp parallelization
+          parLoop.addOMPStatements += "lastprivate(" + itVar + ')'
+          parLoop.begin = AssignmentStatement(VariableAccess(itVar, Some(IntegerDatatype())), njuBegin.expression.get, "=")
+          njuBegin.expression = None
+        case loop =>
+          loop.begin = NullStatement
+      }
       res += njuBegin
     }
     res += loop // update fields later
@@ -79,7 +81,7 @@ private final object UnrollInnermost extends PartialFunction[Node, Transformatio
         Duplicate(loop.inc), Duplicate(loop.body), Duplicate(loop.reduction))
     loop.end = njuEnd
     loop.inc = njuIncr
-    loop.body = duplicateStmts(loop.body, itVar, oldInc, loop.isParallel)
+    loop.body = duplicateStmts(loop.body, Knowledge.opt_unroll, itVar, oldInc, loop.isParallel && Knowledge.opt_unroll_interleave)
 
     loop.annotate(SKIP_ANNOT)
     loop.annotate(Unrolling.NO_REM_ANNOT, njuOffset)
@@ -89,36 +91,36 @@ private final object UnrollInnermost extends PartialFunction[Node, Transformatio
       return res
   }
 
-  private def updateIncrement(inc : Statement) : (Statement, String, Long) = {
+  private[optimization] def updateIncrement(inc : Statement, unrollFactor : Int) : (Statement, String, Long) = {
     return inc match {
       case AssignmentStatement(VariableAccess(itVar, Some(IntegerDatatype())),
         AdditionExpression(VariableAccess(itVar2, Some(IntegerDatatype())), IntegerConstant(incr)),
         "=") if (itVar == itVar2) =>
         (AssignmentStatement(VariableAccess(itVar, Some(IntegerDatatype())),
-          IntegerConstant(incr * Knowledge.opt_unroll), "+="), itVar, incr)
+          IntegerConstant(incr * unrollFactor), "+="), itVar, incr)
 
       case AssignmentStatement(VariableAccess(itVar, Some(IntegerDatatype())),
         AdditionExpression(IntegerConstant(incr), VariableAccess(itVar2, Some(IntegerDatatype()))),
         "=") if (itVar == itVar2) =>
         (AssignmentStatement(VariableAccess(itVar, Some(IntegerDatatype())),
-          IntegerConstant(incr * Knowledge.opt_unroll), "+="), itVar, incr)
+          IntegerConstant(incr * unrollFactor), "+="), itVar, incr)
 
       case AssignmentStatement(VariableAccess(itVar, Some(IntegerDatatype())), IntegerConstant(incr),
         "+=") =>
         (AssignmentStatement(VariableAccess(itVar, Some(IntegerDatatype())),
-          IntegerConstant(incr * Knowledge.opt_unroll), "+="), itVar, incr)
+          IntegerConstant(incr * unrollFactor), "+="), itVar, incr)
 
       case AssignmentStatement(VariableAccess(itVar, Some(IntegerDatatype())), IntegerConstant(incr),
         "-=") =>
         (AssignmentStatement(VariableAccess(itVar, Some(IntegerDatatype())),
-          IntegerConstant(incr * Knowledge.opt_unroll), "-="), itVar, incr)
+          IntegerConstant(incr * unrollFactor), "-="), itVar, incr)
 
       case _ =>
         throw new UnrollException("cannot determine stride: " + inc.prettyprint())
     }
   }
 
-  private def updateEnd(end : Expression, itVar : String, offset : Long) : Expression = {
+  private[optimization] def updateEnd(end : Expression, itVar : String, offset : Long) : Expression = {
     return end match {
       case LowerEqualExpression(VariableAccess(itVar2, Some(IntegerDatatype())), bound) if (itVar == itVar2) =>
         LowerEqualExpression(VariableAccess(itVar, Some(IntegerDatatype())), SubtractionExpression(bound, IntegerConstant(offset)))
@@ -137,7 +139,7 @@ private final object UnrollInnermost extends PartialFunction[Node, Transformatio
     }
   }
 
-  private def updateBegin(begin : Statement, itVar : String) : VariableDeclarationStatement = {
+  private[optimization] def updateBegin(begin : Statement, itVar : String) : VariableDeclarationStatement = {
     return begin match {
       case NullStatement =>
         null
@@ -150,21 +152,22 @@ private final object UnrollInnermost extends PartialFunction[Node, Transformatio
     }
   }
 
-  private def duplicateStmts(body : ListBuffer[Statement], itVar : String, oldInc : Long,
-    isParallel : Boolean) : ListBuffer[Statement] = {
+  private def duplicateStmts(body : ListBuffer[Statement], unrollFactor : Int,
+    itVar : String, oldInc : Long, interleave : Boolean) : ListBuffer[Statement] = {
 
     var njuBody : ListBuffer[Statement] = null
     val replaceStrat = new UpdateLoopVarAndNames(itVar)
     val dups = new ListBuffer[Iterator[Statement]]()
 
-    for (i <- 1L until Knowledge.opt_unroll) {
+    for (i <- 1L until unrollFactor) {
       val dup = Duplicate(body)
       replaceStrat.offset = i * oldInc
       replaceStrat.applyStandalone(Scope(dup))
-      dups += dup.iterator.filterNot(s => s.isInstanceOf[CommentStatement])
+      SimplifyExpression.SimplifyIndices.applyStandalone(Scope(dup))
+      dups += dup.iterator.filter(s => !s.isInstanceOf[CommentStatement])
     }
 
-    if (Knowledge.opt_unroll_interleave && isParallel) {
+    if (interleave) {
       njuBody = new ListBuffer[Statement]()
       for (stmt <- body) {
         njuBody += stmt // reuse original statement
@@ -181,8 +184,8 @@ private final object UnrollInnermost extends PartialFunction[Node, Transformatio
     return njuBody
   }
 
-  private class UpdateLoopVarAndNames(itVar : String)
-      extends DefaultStrategy("Add loop var offset and rename declarations") {
+  private[optimization] class UpdateLoopVarAndNames(itVar : String)
+      extends DefaultStrategy("Add loop var offset, rename declarations, and/or add annotation") {
 
     private final val SKIP_ANNOT = "UpLVSkip"
     private val rename = new HashSet[String]()
@@ -191,40 +194,26 @@ private final object UnrollInnermost extends PartialFunction[Node, Transformatio
 
     this += new Transformation("apply", {
       case vAcc @ VariableAccess(v, Some(IntegerDatatype())) if (v == itVar) =>
-        if (!vAcc.removeAnnotation(SKIP_ANNOT).isDefined) {
+        if (offset != 0 && !vAcc.removeAnnotation(SKIP_ANNOT).isDefined) {
           vAcc.annotate(SKIP_ANNOT) // already done
           AdditionExpression(vAcc, IntegerConstant(offset))
         } else
           vAcc
 
-      case vAcc : VariableAccess =>
-        if (rename.contains(vAcc.name))
-          vAcc.name += "_" + offset
+      case vAcc : VariableAccess if (rename.contains(vAcc.name)) =>
+        vAcc.name += "_" + offset
         vAcc
 
-      case decl : VariableDeclarationStatement =>
+      case decl : VariableDeclarationStatement if (offset > 0) =>
         rename.add(decl.name)
         decl.name += "_" + offset
         decl
     })
 
-    private object IndexCleaner extends Collector {
-      override def enter(node : Node) : Unit = {}
-      override def leave(node : Node) : Unit = {
-        node match {
-          case a : ArrayAccess => a.index = SimplifyExpression.simplifyIntegralExpr(a.index)
-          case _               =>
-        }
-      }
-      override def reset() : Unit = {}
-    }
-
     override def applyStandalone(node : Node) : Unit = {
       val oldLvl = Logger.getLevel
       Logger.setLevel(1)
-      StateManager.register(IndexCleaner) // clean index expressions after modification in replaceStrat
       super.applyStandalone(node)
-      StateManager.unregister(IndexCleaner)
       Logger.setLevel(oldLvl)
     }
   }
