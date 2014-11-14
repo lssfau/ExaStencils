f29f8856cb763138ba5498630fb3f06a749776a0
Kronawitter Stefan <kronast@fim.uni-passau.de>
2014-11-14 09:57:32 +0100

    fix bug in testscripts that prevents recognizing new commits

--- Testing/tests0_git.sh
+++ Testing/tests0_git.sh
@@ -44,31 +44,32 @@ echo "$(date -R):  Initialize tests..." >> "${LOG}"
 
 GIT_URL="ssh://git@git.infosun.fim.uni-passau.de/exastencils/dev/ScalaExaStencil.git"
 if [[ -d "${REPO_DIR}" ]]; then
-  srun git -C "${REPO_DIR}" fetch "${GIT_URL}"
+  OLD_HASH=$(git -C "${REPO_DIR}" rev-parse @)
+  srun git -C "${REPO_DIR}" pull --force
       if [[ $? -ne 0 ]]; then
-        echo "  git remote update failed." >> "${LOG}"
+        echo "= git remote update failed." >> "${LOG}"
         echo "git remote update failed." | mail -s "${FAILURE_SUBJECT}" ${FAILURE_MAIL}
         finish
       fi
-  if [[ $(git -C ${REPO_DIR} rev-parse @) = $(git -C ${REPO_DIR} rev-parse @{u}) ]]; then
+  NEW_HASH=$(git -C "${REPO_DIR}" rev-parse @)
+  if [[ ${OLD_HASH} = ${NEW_HASH} ]]; then
     # up-to-date, no need to run tests, exit script
     echo "  No changes since last test runs, finishing." >> "${LOG}"
     finish
-  else
-    echo "  New commits found, pull" >> "${LOG}"
-    srun git -C "${REPO_DIR}" pull --force "${GIT_URL}"
   fi
 else
   echo "  No local repo found, create a new clone." >> "${LOG}"
   mkdir -p "${REPO_DIR}"
   srun git clone "${GIT_URL}" "${REPO_DIR}"
       if [[ $? -ne 0 ]]; then
-        echo "  git clone failed." >> "${LOG}"
+        echo "= git clone failed." >> "${LOG}"
         echo "git clone failed." | mail -s "${FAILURE_SUBJECT}" ${FAILURE_MAIL}
         finish
       fi
 fi
 
 mkdir -p "${TEMP_DIR}"
+NEW_HASH=$(git -C "${REPO_DIR}" rev-parse @)
+echo "  Run tests for hash  ${NEW_HASH}." >> "${LOG}"
 sbatch "${REPO_DIR}/Testing/tests1_all.sh" "${REPO_DIR}" "${TEMP_DIR}" "${LOG}"
 finish
