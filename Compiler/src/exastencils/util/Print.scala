64610561b55fd24bfbe6a97cafd5aa7a2b361fa2
kuckuk <sebastian.kuckuk@fau.de>
2015-02-02 10:18:46 +0100

    - print field should now work without MPI as well - updated MPI_Sequential

--- Compiler/src/exastencils/util/Print.scala
+++ Compiler/src/exastencils/util/Print.scala
@@ -25,31 +25,36 @@ case class PrintFieldStatement(var filename : Expression, var field : FieldSelec
   override def prettyprint(out : PpStream) : Unit = out << "NOT VALID ; CLASS = PrintFieldStatement\n"
 
   override def expand : Output[StatementList] = {
-    // FIXME: this has to be adapted for non-mpi
-    var statements : ListBuffer[Statement] = ListBuffer()
-
-    statements += new ConditionStatement(new MPI_IsRootProc,
-      ListBuffer[Statement](
-        "std::ofstream stream(" ~ filename ~ ", std::ios::trunc)",
-        "stream.close()"))
-
     var access = new FieldAccess(field, LoopOverDimensions.defIt)
     access.index(Knowledge.dimensionality) = 0
-    statements += new MPI_Sequential(ListBuffer[Statement](
-      new LoopOverFragments(
-        new ConditionStatement(iv.IsValidForSubdomain(field.domainIndex),
-          ListBuffer[Statement](
-            "std::ofstream stream(" ~ filename ~ ", std::ios::app)",
-            new LoopOverDimensions(Knowledge.dimensionality + 1, new IndexRange(
-              new MultiIndex((0 until Knowledge.dimensionality + 1).toArray.map(i => (field.fieldLayout(i).idxDupLeftBegin - field.referenceOffset(i)) : Expression)),
-              new MultiIndex((0 until Knowledge.dimensionality + 1).toArray.map(i => (field.fieldLayout(i).idxDupRightEnd - field.referenceOffset(i)) : Expression))),
-              ListBuffer[Statement](
-                new InitGeomCoords(field.field, false),
-                ("stream << xPos << \" \"" +
-                  (if (Knowledge.dimensionality > 1) " << yPos << \" \"" else "") +
-                  (if (Knowledge.dimensionality > 2) " << zPos << \" \"" else "") +
-                  " << " : Expression) ~ access ~ " << std::endl")),
-            "stream.close()")))))
+
+    var innerLoop = new LoopOverFragments(
+      new ConditionStatement(iv.IsValidForSubdomain(field.domainIndex),
+        ListBuffer[Statement](
+          "std::ofstream stream(" ~ filename ~ ", " ~ (if (Knowledge.useMPI) "std::ios::app" else "std::ios::trunc") ~ ")",
+          new LoopOverDimensions(Knowledge.dimensionality + 1, new IndexRange(
+            new MultiIndex((0 until Knowledge.dimensionality + 1).toArray.map(i => (field.fieldLayout(i).idxDupLeftBegin - field.referenceOffset(i)) : Expression)),
+            new MultiIndex((0 until Knowledge.dimensionality + 1).toArray.map(i => (field.fieldLayout(i).idxDupRightEnd - field.referenceOffset(i)) : Expression))),
+            ListBuffer[Statement](
+              new InitGeomCoords(field.field, false),
+              ("stream << xPos << \" \"" +
+                (if (Knowledge.dimensionality > 1) " << yPos << \" \"" else "") +
+                (if (Knowledge.dimensionality > 2) " << zPos << \" \"" else "") +
+                " << " : Expression) ~ access ~ " << std::endl")),
+          "stream.close()")))
+
+    var statements : ListBuffer[Statement] = ListBuffer()
+
+    if (Knowledge.useMPI) {
+      statements += new ConditionStatement(new MPI_IsRootProc,
+        ListBuffer[Statement](
+          "std::ofstream stream(" ~ filename ~ ", std::ios::trunc)",
+          "stream.close()"))
+
+      statements += new MPI_Sequential(innerLoop)
+    } else {
+      statements += innerLoop
+    }
 
     statements
   }
