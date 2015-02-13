8594e278b80baac243e47f32ba3476d580450a76
Christian Schmitt <christian.schmitt@cs.fau.de>
2015-02-13 09:37:01 +0100

    resolve leveled global values

--- Compiler/src/exastencils/core/collectors/L4ValueCollector.scala
+++ Compiler/src/exastencils/core/collectors/L4ValueCollector.scala
@@ -6,48 +6,44 @@ import exastencils.datastructures.l4._
 import scala.collection.mutable.HashMap
 
 class L4ValueCollector extends Collector {
-  //  private var curLevel = -1
-  private var global : GlobalDeclarationStatement = null
   private var values = new HashMap[String, Expression]()
+  private var globalVals = HashMap[String, Expression]()
 
   override def enter(node : Node) : Unit = {
     node match {
-      case x : GlobalDeclarationStatement => global = x
-      case x : FunctionStatement          => values.clear()
-      case x : ValueDeclarationStatement  => values.+=((x.identifier.name, x.expression))
-      //      case FunctionStatement(LeveledIdentifier(_, SingleLevelSpecification(level)), _, _, _) => curLevel = level
-      case _                              =>
+      case x : GlobalDeclarationStatement => {
+        x.values.foreach(v => v.identifier match {
+          case vl : LeveledIdentifier => globalVals += ((v.identifier.name + "_" + vl.level, v.expression))
+          case _                      => globalVals += ((v.identifier.name, v.expression))
+        })
+        x.variables.filter(_.expression.isDefined).foreach(v => v.identifier match {
+          case vl : LeveledIdentifier => globalVals += ((v.identifier.name + "_" + vl.level, v.expression.get))
+          case _                      => globalVals += ((v.identifier.name, v.expression.get))
+        })
+      }
+      case x : FunctionStatement => values.clear()
+      case _                     =>
     }
   }
 
   override def leave(node : Node) : Unit = {
     node match {
       case x : FunctionStatement => values.clear()
-      //      case FunctionStatement(LeveledIdentifier(_, SingleLevelSpecification(level)), _, _, _) => // due to duplication of functions, functions can be left that were never entered
       case _                     =>
     }
   }
 
   override def reset() : Unit = {
-    //    curLevel = -1
     values.clear()
   }
 
   def getValue(name : String) : Option[Expression] = {
-    var decl : Option[ValueDeclarationStatement] = None
-    if (global != null) {
-      decl = global.values.find(_.identifier.name == name)
-    }
-    if (decl.isDefined) {
-      Some(decl.get.expression)
-    } else {
-      values.get(name)
-    }
-  }
+    var exp : Option[Expression] = None
 
-  //  def getCurrentLevel : Int = {
-  //    if (curLevel < 0)
-  //      Logger.dbg("Trying to access level outside of a valid scope")
-  //    curLevel
-  //  }
+    exp = values.get(name) // Local Vals will shadow global Vals
+    if (!exp.isDefined) {
+      exp = globalVals.get(name)
+    }
+    exp
+  }
 }
