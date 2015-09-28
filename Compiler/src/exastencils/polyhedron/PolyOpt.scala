49b3eae863d389807885f71c7ef5729a07035542
Stefan Kronawitter <kronast@fim.uni-passau.de>
2015-09-28 16:34:49 +0200

    fix interaction of scalar merging with dead code elimination; better code format (remove one unnecessary indentation)

--- Compiler/src/exastencils/polyhedron/PolyOpt.scala
+++ Compiler/src/exastencils/polyhedron/PolyOpt.scala
@@ -124,95 +124,104 @@ object PolyOpt extends CustomStrategy("Polyhedral optimizations") {
         found = true
         sc
     })
-    for (decl : VariableDeclarationStatement <- scop.decls) {
-      Breaks.breakable {
-        val name : String = decl.name
-        toFind = name
-        var fstStmt : Int = -1
-        var lstStmt : Int = -1
-        var stmts : Buffer[(String, (ListBuffer[Statement], ArrayBuffer[String]))] =
-          scop.stmts.toBuffer.sorted(new Ordering[(String, _)] {
-            override def compare(a : (String, _), b : (String, _)) : Int = {
-              return a._1.compareTo(b._1)
-            }
-          })
-        var i : Int = 0
-        val oldLvl = Logger.getLevel
-        Logger.setLevel(Logger.WARNING)
-        for ((lab, (stmt, _)) <- stmts) {
-          found = false
-          this.execute(search, Some(Scope(stmt)))
-          if (found) {
-            if (fstStmt < 0)
-              fstStmt = i
-            lstStmt = i
+    for (decl : VariableDeclarationStatement <- scop.decls) Breaks.breakable {
+      val name : String = decl.name
+      toFind = name
+      var fstStmt : Int = -1
+      var lstStmt : Int = -1
+      var stmts : Buffer[(String, (ListBuffer[Statement], ArrayBuffer[String]))] =
+        scop.stmts.toBuffer.sorted(new Ordering[(String, _)] {
+          override def compare(a : (String, _), b : (String, _)) : Int = {
+            return a._1.compareTo(b._1)
           }
-          i += 1
+        })
+      var i : Int = 0
+      val oldLvl = Logger.getLevel
+      Logger.setLevel(Logger.WARNING)
+      for ((lab, (stmt, _)) <- stmts) {
+        found = false
+        this.execute(search, Some(Scope(stmt)))
+        if (found) {
+          if (fstStmt < 0)
+            fstStmt = i
+          lstStmt = i
         }
-        Logger.setLevel(oldLvl)
-        stmts = stmts.slice(fstStmt, lstStmt + 1)
-        // check if all statements have the same iteration space
-        val remDoms = new ArrayBuffer[isl.Set]()
-        var njuDomain : isl.UnionSet = null
-        scop.domain.foreachSet({
-          set : isl.Set =>
-            var found : Boolean = false
-            for ((lab, (stmt, _)) <- stmts)
-              if (set.getTupleName() == lab)
-                found = true
-            if (found)
-              remDoms += set
-            else
-              njuDomain = if (njuDomain == null) set else njuDomain.addSet(set)
-            () // return type must be Unit, but the true branch returns a Buffer...
-        })
-        var proto : isl.Set = remDoms(0).resetTupleId()
-        for (i <- 1 until remDoms.length)
-          if (!proto.isEqual(remDoms(i).resetTupleId()))
-            Breaks.break() // continue... different domains, cannot merge statements
-        val mergedStmts = new ListBuffer[Statement]()
-        var mergedLoopIts : ArrayBuffer[String] = null
-        for ((lab, (stmt, loopIts)) <- stmts) {
-          mergedStmts ++= stmt
-          if (mergedLoopIts == null)
-            mergedLoopIts = loopIts
-          else if (!mergedLoopIts.sameElements(loopIts))
-            Breaks.break() // continue... loopIts must be identical?!
-        }
-        scop.domain =
-          if (njuDomain == null) remDoms(0)
-          else njuDomain.addSet(remDoms(0)) // re-add one of the domains
-        val njuLabel : String = remDoms(0).getTupleName()
-        for ((lab, _) <- stmts)
-          scop.stmts(lab) = (mergedStmts, mergedLoopIts)
-        // adjust read and write accesses
-        var nju : isl.UnionMap = null
-        scop.reads.foreachMap({
-          map : isl.Map =>
-            if (map.getTupleName(isl.DimType.Out) != name) { // remove all accesses to the scalar
-              val oldLabel : String = map.getTupleName(isl.DimType.In)
-              var toAdd : isl.Map = map
-              if (oldLabel != njuLabel)
-                for ((lab, _) <- stmts if (oldLabel == lab))
-                  toAdd = toAdd.setTupleName(isl.DimType.In, njuLabel)
-              nju = if (nju == null) toAdd else nju.addMap(toAdd)
-            }
-        })
-        scop.reads = nju
-        nju = null
-        scop.writes.foreachMap({
-          map : isl.Map =>
-            if (map.getTupleName(isl.DimType.Out) != name) { // remove all accesses to the scalar
-              val oldLabel : String = map.getTupleName(isl.DimType.In)
-              var toAdd : isl.Map = map
-              if (oldLabel != njuLabel)
-                for ((lab, _) <- stmts if (oldLabel == lab))
-                  toAdd = toAdd.setTupleName(isl.DimType.In, njuLabel)
-              nju = if (nju == null) toAdd else nju.addMap(toAdd)
-            }
-        })
-        scop.writes = nju
+        i += 1
       }
+      Logger.setLevel(oldLvl)
+      stmts = stmts.slice(fstStmt, lstStmt + 1)
+      // check if all statements have the same iteration space
+      val remDoms = new ArrayBuffer[isl.Set]()
+      var njuDomain : isl.UnionSet = null
+      scop.domain.foreachSet({
+        set : isl.Set =>
+          var found : Boolean = false
+          for ((lab, (stmt, _)) <- stmts)
+            if (set.getTupleName() == lab)
+              found = true
+          if (found)
+            remDoms += set
+          else
+            njuDomain = if (njuDomain == null) set else njuDomain.addSet(set)
+          () // return type must be Unit, but the true branch returns a Buffer...
+      })
+      var proto : isl.Set = remDoms(0).resetTupleId()
+      for (i <- 1 until remDoms.length)
+        if (!proto.isEqual(remDoms(i).resetTupleId()))
+          Breaks.break() // continue... different domains, cannot merge statements
+      val mergedStmts = new ListBuffer[Statement]()
+      var mergedLoopIts : ArrayBuffer[String] = null
+      for ((lab, (stmt, loopIts)) <- stmts) {
+        mergedStmts ++= stmt
+        if (mergedLoopIts == null)
+          mergedLoopIts = loopIts
+        else if (!mergedLoopIts.sameElements(loopIts))
+          Breaks.break() // continue... loopIts must be identical?!
+      }
+      scop.domain =
+        if (njuDomain == null) remDoms(0)
+        else njuDomain.addSet(remDoms(0)) // re-add one of the domains
+      val njuLabel : String = remDoms(0).getTupleName()
+      for ((lab, _) <- stmts)
+        if (lab == njuLabel)
+          scop.stmts(lab) = (mergedStmts, mergedLoopIts)
+        else
+	  scop.stmts -= lab
+      // adjust read and write accesses
+      var nju : isl.UnionMap = null
+      scop.reads.foreachMap({
+        map : isl.Map =>
+          if (map.getTupleName(isl.DimType.Out) != name) { // remove all accesses to the scalar
+            val oldLabel : String = map.getTupleName(isl.DimType.In)
+            var toAdd : isl.Map = map
+            if (oldLabel != njuLabel)
+              for ((lab, _) <- stmts if (oldLabel == lab))
+                toAdd = toAdd.setTupleName(isl.DimType.In, njuLabel)
+            nju = if (nju == null) toAdd else nju.addMap(toAdd)
+          }
+      })
+      scop.reads = nju
+      nju = null
+      scop.writes.foreachMap({
+        map : isl.Map =>
+          if (map.getTupleName(isl.DimType.Out) != name) { // remove all accesses to the scalar
+            val oldLabel : String = map.getTupleName(isl.DimType.In)
+            var toAdd : isl.Map = map
+            if (oldLabel != njuLabel)
+              for ((lab, _) <- stmts if (oldLabel == lab))
+               toAdd = toAdd.setTupleName(isl.DimType.In, njuLabel)
+            nju = if (nju == null) toAdd else nju.addMap(toAdd)
+          }
+      })
+      scop.writes = nju
+
+      // update scop.deadAfterScop
+      var resurrect : Boolean = false
+      for (set <- remDoms)
+        if (scop.deadAfterScop.intersect(set).isEmpty())
+          resurrect = true
+      if (resurrect)
+        scop.deadAfterScop = scop.deadAfterScop.subtract(isl.Set.universeLike(remDoms(0)))
     }
   }
 
