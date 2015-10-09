019fbc672d4a892a2a45c464dc7c4c939206bf91
kuckuk <sebastian.kuckuk@fau.de>
2015-10-09 09:51:38 +0200

    - cleaned up my local configs

--- Configs/Sebastian/Generate_LSS.bat
+++
@@ -1,5 +0,0 @@
-set configurations=2_6_0_4x4x4_1x1x1 2_26_0_4x4x4_1x1x1 2_6_0_2x2x2_2x2x2 2_26_0_2x2x2_2x2x2 2_6_1_2x2x2_2x2x2 2_26_1_2x2x2_2x2x2
-
-for %%c in (%configurations%) do (
-	java.exe -cp "C:\Eclipse\plugins\org.scala-lang.scala-library_2.11.2.v20140721-095018-73fb460c1c.jar;C:\Eclipse\plugins\org.scala-lang.scala-reflect_2.11.2.v20140721-095018-73fb460c1c.jar;.\Compiler\bin;.\CompilerMacros\CompilerMacros\bin;.\Compiler\lib\*" Main .\Configs\Sebastian\LSS_%%c_Settings.txt .\Configs\Sebastian\LSS_%%c_Knowledge.txt
-)
---
+++ Configs/Sebastian/generate_i10anim.bat
@@ -0,0 +1,2 @@
+java.exe -Xmx2G -Xms2G -cp "C:\Eclipse\plugins\*;.\Compiler\bin;.\CompilerMacros\CompilerMacros\bin;.\Compiler\lib\*" Main .\Configs\Sebastian\i10anim.settings .\Configs\Sebastian\i10anim.knowledge
+
