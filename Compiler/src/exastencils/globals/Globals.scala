cace4d1be5039cd9b6dd12b30d8d273bd2e93c8a
kuckuk <sebastian.kuckuk@fau.de>
2015-10-22 09:24:53 +0200

    - added missing include

--- Compiler/src/exastencils/globals/Globals.scala
+++ Compiler/src/exastencils/globals/Globals.scala
@@ -7,7 +7,7 @@ import exastencils.knowledge._
 import exastencils.prettyprinting._
 
 case class Globals(var variables : ListBuffer[VariableDeclarationStatement] = new ListBuffer) extends FunctionCollection("Globals/Globals",
-  ListBuffer(),
+  ListBuffer("algorithm"), // provides commonly used functions like min/max
   ListBuffer("Util/Vector.h", "Util/Matrix.h") /*
     ++ Settings.additionalIncludes*/ ,
   ListBuffer(
