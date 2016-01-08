b518a6187a793c1966ba7550e7615c681d51c67a
kuckuk <sebastian.kuckuk@fau.de>
2016-01-08 10:26:59 +0100

    - allow 'communicate StencilFieldAccess' on L4

--- Compiler/src/exastencils/datastructures/l4/Communication.scala
+++ Compiler/src/exastencils/datastructures/l4/Communication.scala
@@ -8,13 +8,13 @@ import exastencils.knowledge
 import exastencils.prettyprinting._
 
 case class CommunicateTarget(var target : String, var begin : Option[Index], var end : Option[Index]) extends Expression {
-  def prettyprint(out : PpStream) = {
+  override def prettyprint(out : PpStream) = {
     out << target
     if (begin.isDefined) out << ' ' << begin.get
     if (end.isDefined) out << " to " << end.get
   }
 
-  def progressToIr : communication.CommunicateTarget = {
+  override def progressToIr : communication.CommunicateTarget = {
     communication.CommunicateTarget(
       target,
       if (begin.isDefined) Some(begin.get.progressToIr) else None,
@@ -23,9 +23,9 @@ case class CommunicateTarget(var target : String, var begin : Option[Index], var
 }
 
 case class ApplyBCsStatement(var field : Access) extends Statement {
-  def prettyprint(out : PpStream) = { out << "apply bc to " << field << '\n' }
+  override def prettyprint(out : PpStream) = { out << "apply bc to " << field << '\n' }
 
-  def progressToIr : communication.ApplyBCsStatement = {
+  override def progressToIr : communication.ApplyBCsStatement = {
     val resolvedField = field match {
       case f : FieldAccess => f.progressToIr.fieldSelection
       case sf : StencilFieldAccess => knowledge.FieldSelection(sf.resolveField,
@@ -38,16 +38,22 @@ case class ApplyBCsStatement(var field : Access) extends Statement {
 }
 
 case class CommunicateStatement(var field : Access, var op : String, var targets : List[CommunicateTarget]) extends Statement {
-  def prettyprint(out : PpStream) = {
+  override def prettyprint(out : PpStream) = {
     out <<
       (if ("both" == op) "" else (op + ' ')) <<
       "communicate " <<< (targets, " ") << (if (targets.isEmpty) "" else " of ") <<
       field << '\n'
   }
 
-  def progressToIr : communication.CommunicateStatement = {
+  override def progressToIr : communication.CommunicateStatement = {
+    val progressedField = field match {
+      case f : FieldAccess => f.progressToIr.fieldSelection
+      case sf : StencilFieldAccess => knowledge.FieldSelection(sf.resolveField,
+        ir.IntegerConstant(sf.level.asInstanceOf[SingleLevelSpecification].level),
+        FieldAccess.resolveSlot(sf.resolveField, sf.slot),
+        sf.arrayIndex)
+    }
     val progressedTargets : ListBuffer[communication.CommunicateTarget] = ListBuffer()
-    val progressedField = field.asInstanceOf[FieldAccess].progressToIr.fieldSelection
 
     if (targets.isEmpty)
       progressedTargets += CommunicateTarget("all", None, None).progressToIr
