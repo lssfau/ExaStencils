69e4c8283d483b09de79964317cb247d470b532e
kuckuk <sebastian.kuckuk@fau.de>
2016-02-09 16:22:41 +0100

    - fix timer name and caption generation

--- Compiler/src/exastencils/datastructures/ir/iv/Util.scala
+++ Compiler/src/exastencils/datastructures/ir/iv/Util.scala
@@ -4,11 +4,12 @@ import exastencils.datastructures.ir._
 import exastencils.datastructures.ir.ImplicitConversions._
 
 case class Timer(var name : Expression) extends UnduplicatedVariable {
-  // TODO: strip result of resolveName (no spaces, etc.)
-  override def resolveName = s"timer_" + name.prettyprint
+  override def resolveName = s"timer_" + stripName
   override def resolveDataType = "StopWatch"
 
+  def stripName = name.prettyprint.replaceAll("[^a-zA-Z0-9]", "_")
+
   override def getCtor() : Option[Statement] = {
-    Some(AssignmentStatement(resolveName ~ ".timerName", s""""${name}""""))
+    Some(AssignmentStatement(resolveName ~ ".timerName", StringConstant(stripName)))
   }
 }
