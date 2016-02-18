082fa7974778d641c9875e7c4886f51c99bb99e9
Christian Schmitt <christian.schmitt@cs.fau.de>
2016-02-18 12:59:01 +0100

    intermediate commit

--- Compiler/src/exastencils/datastructures/l1/Domain.scala
+++ Compiler/src/exastencils/datastructures/l1/Domain.scala
@@ -1,5 +1,12 @@
 package exastencils.datastructures.l1
 
+import exastencils.logger._
+import exastencils.knowledge._
 import exastencils.datastructures.l1._
 
-case class Domain(var identifier : String, var range : List[Tuple2[Double, Double]]) extends Definition
+case class Domain(var identifier : String, var range : List[Tuple2[Double, Double]]) extends Definition {
+  range.length match {
+    case (1 | 2 | 3) => Knowledge.dimensionality = range.length
+    case _           => Logger.error(s"""Dimensionality of ${range.length} not supported.""");
+  }
+}
