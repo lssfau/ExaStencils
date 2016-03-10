8fc3828d512fc0c242ab4f6f32d6e3933f0d5d2f
Christian Schmitt <christian.schmitt@cs.fau.de>
2016-03-10 16:01:26 +0100

    Add documentation to Duplicate

--- Compiler/src/exastencils/core/Duplicate.scala
+++ Compiler/src/exastencils/core/Duplicate.scala
@@ -3,17 +3,46 @@ package exastencils.core
 import java.lang.reflect._
 import scala.collection.immutable.Nil
 
+/**
+  * Support object used to (deep) clone object instances.
+  */
 object Duplicate {
-  val cloner = new com.rits.cloning.Cloner
+  private final val cloner = new com.rits.cloning.Cloner
   cloner.setDumpClonedClasses(Settings.printClonedObjects);
 
+  /**
+    * Deep clones the given object instance.
+    *
+    * @param t The object instance to be cloned.
+    * @return The cloned instance.
+    */
   def apply[T](t : T) : T = cloner.deepClone(t)
 
+  /**
+    * Deep clones the given object instance multiple times.
+    *
+    * @param t The object instance to be cloned.
+    * @param n The number of clones to be created.
+    * @return A [[scala.collection.immutable.List]] holding the cloned instances.
+    */
+  def apply[T](t : T, n : Integer) : List[T] = List.fill(n)(cloner.deepClone(t))
+
+  /**
+    * Enable or disable debug output.
+    *
+    * @param output true if debug output should be printed, otherwise false.
+    */
   def debugOutput(output : Boolean) = cloner.setDumpClonedClasses(output)
 
   def dontClone(t : Class[_]) = cloner.dontClone(t)
   def dontCloneHierarchy(t : Class[_]) = cloner.dontCloneInstanceOf(t)
   def nullInsteadOfClone(t : Class[_]) = cloner.nullInsteadOfClone(t)
+
+  /**
+    * Register a class as immutable, which does not need to be cloned.
+    *
+    * @param t The class to be registered as immutable.
+    */
   def registerImmutable(t : Class[_]) = cloner.registerImmutable(t)
   def registerConstant(t : Class[_], field : String) = cloner.registerConstant(t, field)
   def registerConstant(field : Any) = cloner.registerConstant(field)
