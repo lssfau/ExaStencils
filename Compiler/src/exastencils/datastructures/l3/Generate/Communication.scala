8f86148def7fd7dc6145df5f6fcd6008772f09ac
Sebastian Kuckuk <sebastian.kuckuk@fau.de>
2016-05-12 16:19:50 +0200

    improve comm timer naming and handling

--- Compiler/src/exastencils/datastructures/l3/Generate/Communication.scala
+++ Compiler/src/exastencils/datastructures/l3/Generate/Communication.scala
@@ -1,16 +1,21 @@
 package exastencils.datastructures.l3
 
+import scala.collection.mutable.HashMap
+
 import exastencils.knowledge._
 
 object Communication {
+  var commTimerNames = HashMap[String, String]() // TimerName -> label
+
   def applyBCs(printer : java.io.PrintWriter, field : String) = {
     printer.println(s"\tapply bc to $field")
   }
 
   def exch(printer : java.io.PrintWriter, field : String, target : String = "", condition : String = "") = {
     var commName = "communication"
-    if (Knowledge.l3tmp_genCommTimersPerField) commName += s"_$field"
-    if (Knowledge.l3tmp_genCommTimersPerLevel) commName = s"concat ( '$commName', levels@current() )" else s"'$commName'"
+    if (Knowledge.l3tmp_genCommTimersPerField) commName += "_" + field.split('[')(0).split('@')(0)
+    commTimerNames += commName -> (if (Knowledge.l3tmp_genCommTimersPerField) " " + field.split('[')(0).split('@')(0) else "")
+    if (Knowledge.l3tmp_genCommTimersPerLevel) commName = s"concat ( '${commName}_', levels@current() )" else s"'$commName'"
 
     if (Knowledge.l3tmp_genTimersForComm)
       printer.println(s"\tstartTimer ( $commName )")
