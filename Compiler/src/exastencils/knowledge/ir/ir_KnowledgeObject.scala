bbc564125d2975582a7de7d7f113300e9a4fa07a
Sebastian Kuckuk <sebastian.kuckuk@fau.de>
2016-09-28 14:24:59 +0200

    add traits for ir KnowledgeCollection handling

--- Compiler/src/exastencils/knowledge/ir/ir_KnowledgeObject.scala
+++ Compiler/src/exastencils/knowledge/ir/ir_KnowledgeObject.scala
@@ -1,3 +1,13 @@
 package exastencils.knowledge.ir
 
-trait IR_KnowledgeObject
+trait IR_KnowledgeObject {}
+
+trait IR_KnowledgeObjectWithIdent extends IR_KnowledgeObject {
+  def identifier : String
+}
+
+trait IR_KnowledgeObjectWithLevel extends IR_KnowledgeObjectWithIdent {
+  def level : Int
+}
+
+trait IR_KnowledgeObjectWithIdentAndLevel extends IR_KnowledgeObjectWithIdent with IR_KnowledgeObjectWithLevel
