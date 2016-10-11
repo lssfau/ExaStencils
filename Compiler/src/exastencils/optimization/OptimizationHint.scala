8dd947fa0ee2e255f07000c0892072b2b0c30784
Sebastian Kuckuk <sebastian.kuckuk@fau.de>
2016-10-11 09:37:14 +0200

    fuse OptimizationHint's isParallel and isVectorizable with ir ParallelizationInfo => inner ir Loops now remain parallel when expanding LoopOverDimensions

--- Compiler/src/exastencils/optimization/OptimizationHint.scala
+++ Compiler/src/exastencils/optimization/OptimizationHint.scala
@@ -6,8 +6,6 @@ import exastencils.base.ir.IR_VariableAccess
 
 @deprecated("to be integrated with loop nodes", "10.10.2016")
 trait OptimizationHint {
-  var isParallel : Boolean = false
-  var isVectorizable : Boolean = false
   val privateVars = new ListBuffer[IR_VariableAccess]()
   var isInnermost : Boolean = false
 }
--- Compiler/src/exastencils/parallelization/ir/ir_ParallelizationInfo.scala
+++ Compiler/src/exastencils/parallelization/ir/ir_ParallelizationInfo.scala
@@ -5,17 +5,24 @@ import exastencils.base.ir._
 /// IR_ParallelizationInfo
 
 object IR_ParallelizationInfo {
-  def apply() = new IR_ParallelizationInfo(false, 1, None)
+  def apply() = new IR_ParallelizationInfo(false, false, 1, None)
 
   def PotentiallyParallel() = {
     val info = this ()
     info.potentiallyParallel = true
     info
   }
+
+  def Vectorizable() = {
+    val info = this ()
+    info.isVectorizable = true
+    info
+  }
 }
 
 case class IR_ParallelizationInfo(
     var potentiallyParallel : Boolean,
+    var isVectorizable : Boolean,
     var collapseDepth : Int,
     var reduction : Option[IR_Reduction]
 ) extends IR_Node {}
