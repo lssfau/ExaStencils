daf6e63c1532dde690405add7e8ac8cf04ca4a3f
Sebastian Kuckuk <sebastian.kuckuk@fau.de>
2016-11-10 16:50:00 +0100

    print advance statements of unresolved l4 accesses

--- Compiler/src/exastencils/field/l4/L4_Slot.scala
+++ Compiler/src/exastencils/field/l4/L4_Slot.scala
@@ -3,6 +3,7 @@ package exastencils.field.l4
 import exastencils.base.ir.IR_StringLiteral
 import exastencils.base.l4._
 import exastencils.baseExt.ir.IR_LoopOverFragments
+import exastencils.baseExt.l4.L4_UnresolvedAccess
 import exastencils.field.ir._
 import exastencils.logger.Logger
 import exastencils.prettyprinting._
@@ -46,8 +47,16 @@ case class L4_AdvanceSlot(var field : L4_Access) extends L4_Statement {
   override def prettyprint(out : PpStream) = {
     // don't use field's prettyprinter -> slot modifiers are not required in advance statements
     field match {
-      case access : L4_FieldAccess => out << "advance " << access.target.name << "@" << access.target.level
-      case access                  => Logger.warn("Trying to advance slot of something that is not a field: " + access)
+      case access : L4_FieldAccess =>
+        out << "advance " << access.target.name << "@" << access.target.level
+
+      case access : L4_UnresolvedAccess =>
+        out << "advance " << field.name
+        if (access.level.isDefined) out << "@" << access.level.get
+
+      case access =>
+        Logger.warn("Trying to advance slot of something that is not a field: " + access)
+        out << "advance " << access
     }
   }
 
