8152cd1a30fb7796abb10be30a1d780c322e2d49
Sebastian Kuckuk <sebastian.kuckuk@fau.de>
2017-08-17 09:06:45 +0200

    add simplified version of VectorExpression for l2 and l3

---
+++ Compiler/src/exastencils/baseExt/l3/L3_VectorExpression.scala
@@ -0,0 +1,14 @@
+package exastencils.baseExt.l3
+
+import scala.collection.mutable.ListBuffer
+
+import exastencils.base.l3._
+import exastencils.baseExt.l4.L4_VectorExpression
+import exastencils.prettyprinting.PpStream
+
+/// L3_VectorExpression
+
+case class L3_VectorExpression(var entries : ListBuffer[L3_Expression], var rowVector : Boolean = true) extends L3_Expression {
+  override def prettyprint(out : PpStream) : Unit = ???
+  override def progress = L4_VectorExpression(None, entries.map(_.progress).toList, rowVector)
+}
