21eb21272b89816edb453c5a7c2b2ab67b5d5999
Sebastian Kuckuk <sebastian.kuckuk@fau.de>
2017-10-10 12:28:23 +0900

    extract configList from generateExamples.sh add compileExamples.sh and runExamples.sh

---
+++ Examples/examples.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+configList=""
+
+# Stokes
+
+configList+="Stokes/2D_FD_Stokes "
+configList+="Stokes/2D_FV_Stokes "
+
+# Poisson
+
+configList+="Poisson/2D_FD_Poisson "
+configList+="Poisson/2D_FV_Poisson "
+configList+="Poisson/3D_FD_Poisson "
+configList+="Poisson/3D_FV_Poisson "
+
+# OpticalFlow
+
+configList+="OpticalFlow/2D_FD_OptFlow "
+configList+="OpticalFlow/3D_FD_OptFlow "
+configList+="OpticalFlow/2D_FD_OptFlow_onlyL4 "
+configList+="OpticalFlow/2D_FD_OptFlow_onlyL4_Vec "
+configList+="OpticalFlow/3D_FD_OptFlow_onlyL4_Vec "
+
+# SWE
+
+configList+="SWE/2D_FV_SWE "
--- Examples/generateExamples.sh
+++ Examples/generateExamples.sh
@@ -2,28 +2,11 @@
 
 platform=lib/linux.platform
 
-# Stokes
+source examples.sh
 
-java -cp ../Compiler/Compiler.jar Main Stokes/2D_FD_Stokes.settings Stokes/2D_FD_Stokes.knowledge $platform
-java -cp ../Compiler/Compiler.jar Main Stokes/2D_FV_Stokes.settings Stokes/2D_FV_Stokes.knowledge $platform
+echo generating code for $configList
 
-# Poisson
-
-java -cp ../Compiler/Compiler.jar Main Poisson/2D_FD_Poisson.settings Poisson/2D_FD_Poisson.knowledge $platform
-java -cp ../Compiler/Compiler.jar Main Poisson/2D_FV_Poisson.settings Poisson/2D_FV_Poisson.knowledge $platform
-java -cp ../Compiler/Compiler.jar Main Poisson/3D_FD_Poisson.settings Poisson/3D_FD_Poisson.knowledge $platform
-java -cp ../Compiler/Compiler.jar Main Poisson/3D_FV_Poisson.settings Poisson/3D_FV_Poisson.knowledge $platform
-
-# OpticalFlow
-
-java -cp ../Compiler/Compiler.jar Main OpticalFlow/2D_FD_OptFlow.settings OpticalFlow/2D_FD_OptFlow.knowledge $platform
-java -cp ../Compiler/Compiler.jar Main OpticalFlow/3D_FD_OptFlow.settings OpticalFlow/3D_FD_OptFlow.knowledge $platform
-
-java -cp ../Compiler/Compiler.jar Main OpticalFlow/2D_FD_OptFlow_onlyL4.settings OpticalFlow/2D_FD_OptFlow_onlyL4.knowledge $platform
-
-java -cp ../Compiler/Compiler.jar Main OpticalFlow/2D_FD_OptFlow_onlyL4_Vec.settings OpticalFlow/2D_FD_OptFlow_onlyL4_Vec.knowledge $platform
-java -cp ../Compiler/Compiler.jar Main OpticalFlow/3D_FD_OptFlow_onlyL4_Vec.settings OpticalFlow/3D_FD_OptFlow_onlyL4_Vec.knowledge $platform
-
-# SWE
-
-java -cp ../Compiler/Compiler.jar Main SWE/2D_FV_SWE.settings SWE/2D_FV_SWE.knowledge $platform
+for config in $configList; do
+  echo generating $config
+  java -cp ../Compiler/Compiler.jar Main $config.settings $config.knowledge $platform
+done
