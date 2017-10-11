137f65b9f8ec3130780fdf6fee74ae586b776890
Sebastian Kuckuk <sebastian.kuckuk@fau.de>
2017-10-11 13:14:05 +0900

    update examples

--- Examples/compileExamples.sh
+++ Examples/compileExamples.sh
@@ -5,11 +5,13 @@ platform=lib/linux.platform
 source examples.sh
 
 echo compiling code for $configList
+echo 
 
 callPath=$(pwd)
 
 for config in $configList; do
-  echo compiling $config
+  echo compiling $config ...
   cd $callPath/generated/${config##*/}
-  time make -j
+  time make -j 8 > $callPath/Debug/${config##*/}_makeResult.txt
+  echo 
 done
