68ff4e1d6db9ecd41cbee468beb542ab8d9c7a3b
Sebastian Kuckuk <sebastian.kuckuk@fau.de>
2017-11-06 12:00:16 +0100

    move L1_FieldDiscretization to separate file

--- Compiler/src/exastencils/discretization/l1/L1_DiscretizationStatement.scala
+++ Compiler/src/exastencils/discretization/l1/L1_DiscretizationStatement.scala
@@ -1,53 +1,11 @@
 package exastencils.discretization.l1
 
-import scala.collection.mutable.ListBuffer
-
 import exastencils.base.l1._
-import exastencils.field.l1.L1_FieldCollection
-import exastencils.grid.l1.L1_Localization
 import exastencils.logger.Logger
-import exastencils.prettyprinting.PpStream
 
 /// L1_DiscretizationStatement
 
 abstract class L1_DiscretizationStatement extends L1_Statement {
   def process() : Unit
   override def progress = Logger.error(s"Trying to progress L1 discretize statement; this is not supported")
-}
-
-/// L1_FieldDiscretization
-
-object L1_FieldDiscretization {
-  def apply(src : String, levels : Option[L1_DeclarationLevelSpecification], mapping : Option[String], localization : String) =
-    new L1_FieldDiscretization(src, levels, mapping, L1_Localization.resolve(localization))
-}
-
-case class L1_FieldDiscretization(
-    var src : String,
-    var levels : Option[L1_DeclarationLevelSpecification],
-    var mapping : Option[String],
-    var localization : L1_Localization) extends L1_DiscretizationStatement {
-
-  override def prettyprint(out : PpStream) = {
-    out << src
-    if (mapping.isDefined) out << " => " << mapping.get
-    out << " on " << localization
-  }
-
-  override def process() = {
-    val fields = {
-      if (levels.isDefined) // explicit level definition is given -> extract levels and map to fields
-        L1_LevelSpecification.extractLevelListDefEmpty(levels).to[ListBuffer].map(lvl => L1_FieldCollection.getByIdentifier(src, lvl).get)
-      else // otherwise collect all fields with the src name
-        L1_FieldCollection.getAllByIdentifier(src)
-    }
-
-    fields.foreach(l1Field => {
-      l1Field.localization = Some(localization)
-
-      val l2Field = l1Field.getProgressedObj()
-
-      if (mapping.isDefined) l2Field.name = mapping.get
-    })
-  }
 }
