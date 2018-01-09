c3bdd69de80f4dcc37bc89cff0ac27a027a8e729
Stefan Kronawitter <kronast@fim.uni-passau.de>
2018-01-09 14:22:45 +0100

    minor

--- Compiler/src/exastencils/field/ir/IR_AddPaddingToFieldLayouts.scala
+++ Compiler/src/exastencils/field/ir/IR_AddPaddingToFieldLayouts.scala
@@ -10,12 +10,14 @@ object IR_AddPaddingToFieldLayouts {
       // shortcut to innermost layout information
       val innerLayout = fieldLayout.layoutsPerDim(0)
 
+      val vs = Platform.simd_vectorSize
+
       // left padding
-      innerLayout.numPadLayersLeft = (Platform.simd_vectorSize - innerLayout.numGhostLayersLeft % Platform.simd_vectorSize) % Platform.simd_vectorSize
+      innerLayout.numPadLayersLeft = (vs - innerLayout.numGhostLayersLeft % vs) % vs
 
       // right padding
       val total = innerLayout.numPadLayersLeft + innerLayout.numGhostLayersLeft + innerLayout.numDupLayersLeft + innerLayout.numInnerLayers + innerLayout.numDupLayersRight + innerLayout.numGhostLayersRight
-      innerLayout.numPadLayersRight = (Platform.simd_vectorSize - total % Platform.simd_vectorSize) % Platform.simd_vectorSize
+      innerLayout.numPadLayersRight = (vs - total % vs) % vs
 
       // update total number of values in the field
       for (layout <- fieldLayout.layoutsPerDim) layout.updateTotal()
