f2870ba3c8434dae8d546882ce5747b9105df3ab
Stefan Kronawitter <kronast@fim.uni-passau.de>
2018-02-07 10:58:31 +0100

    add generalized SIMD_Scalars2Vector; may be used for scalar portions in vectorized code (IR_Vectorization not updated yet)

--- Compiler/src/exastencils/simd/SIMD_Declaration.scala
+++ Compiler/src/exastencils/simd/SIMD_Declaration.scala
@@ -1,12 +1,15 @@
 package exastencils.simd
 
+import scala.collection.mutable.ListBuffer
+
+import exastencils.base.ir.IR_Expression
+import exastencils.base.ir.IR_IntegerConstant
 import exastencils.config._
 import exastencils.prettyprinting.PpStream
 
-/// SIMD_IncrementVectorDeclaration
+/// SIMD_Scalars2Vector
 
-/** Special declaration for a SIMD vector, which is initialized with the values 0, 1, ..., Knowledge.simd_vectorSize-1. */
-case class SIMD_IncrementVectorDeclaration(var name : String, var incr : Long) extends SIMD_Statement {
+case class SIMD_Scalars2Vector(var name : String, var scalars : ListBuffer[IR_Expression]) extends SIMD_Statement {
   override def prettyprint(out : PpStream) : Unit = {
     out << SIMD_RealDatatype << ' ' << name
     val is = Platform.simd_instructionSet
@@ -14,30 +17,36 @@ case class SIMD_IncrementVectorDeclaration(var name : String, var incr : Long) e
       case "QPX" =>
         out << ";\n"
         out << "{\n"
-        out << " double _a[4] __attribute__((aligned(32))) = { 0, " << incr << ", " << 2 * incr << ", " << 3 * incr << " };\n"
+        out << " double _a[4] __attribute__((aligned(32))) = { " <<< (scalars, ", ") << " };\n"
         out << ' ' << name << " = vec_lda(0, _a);\n"
         out << "}"
 
       case "SSE3" | "AVX" | "AVX2" | "AVX512" =>
         val bit = if (is == "SSE3") "" else if (is == "AVX512") "512" else "256"
         val prec = if (Knowledge.useDblPrecision) 'd' else 's'
-        out << " = _mm" << bit << "_set_p" << prec << '('
-        for (i <- Platform.simd_vectorSize - 1 to 1 by -1)
-          out << i * incr << ", "
-        out << "0);"
+        out << " = _mm" << bit << "_set_p" << prec << '(' <<< (scalars.view.reverse, ", ") << ");"
 
       case "IMCI" =>
-        out << " (" << SIMD_RealDatatype << ") { 0"
-        for (i <- 1 until Platform.simd_vectorSize)
-          out << ", " << i * incr
-        out << " };"
+        out << " = (" << SIMD_RealDatatype << ") { " <<< (scalars, ", ") << " };"
 
       case "NEON" =>
         out << ";\n"
         out << "{\n"
-        out << " float _a[4] = { 0, " << incr << ", " << 2 * incr << ", " << 3 * incr << " };\n"
+        out << " float _a[4] = { " <<< (scalars, ", ") << " };\n"
         out << ' ' << name << " = vld1q_f32(_a);\n"
         out << "}"
     }
+  }
+}
+
+/// SIMD_IncrementVectorDeclaration
+
+/** Special declaration for a SIMD vector, which is initialized with the values 0, 1, ..., Knowledge.simd_vectorSize-1. */
+object SIMD_IncrementVectorDeclaration {
+  def apply(name : String, incr : Long) : SIMD_Scalars2Vector = {
+    val scalars = ListBuffer[IR_Expression]()
+    for (i <- 0 until Platform.simd_vectorSize)
+      scalars += IR_IntegerConstant(incr * i)
+    SIMD_Scalars2Vector(name, scalars)
   }
 }
