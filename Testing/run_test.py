5dd38db13c6407441505ed60568fc01945bf19da
Jonas Schmitt <jonas.schmitt@fau.de>
2019-05-09 11:19:51 +0200

    Included Isl into CI pipeline

--- Testing/run_test.py
+++ Testing/run_test.py
@@ -7,7 +7,7 @@ def generate_settings_file(problem_name: str, output_path: str, exa_file_names: 
     for name in exa_file_names:
         for i in range(1, 5):
             if f'.exa{i}' in name:
-                tmp += f'l{i}file\t= {problem_name}.exa{i}\n'
+                tmp += f'l{i}file\t = "{problem_name}.exa{i}"\n'
 
     tmp += f'debugL1File\t= "Debug/{problem_name}_debug.exa1"\n'
     tmp += f'debugL2File\t= "Debug/{problem_name}_debug.exa2"\n'
@@ -15,7 +15,7 @@ def generate_settings_file(problem_name: str, output_path: str, exa_file_names: 
     tmp += f'debugL4File\t= "Debug/{problem_name}_debug.exa4"\n\n'
     tmp += f'htmlLogFile\t= "Debug/{problem_name}_log.html"\n\n'
     tmp += f'outputPath\t= "generated/{problem_name}"\n\n'
-    tmp += f'produceHtmlLog\t= true\n'
+    tmp += f'produceHtmlLog\t= false\n'
     tmp += f'timeStrategies\t= true\n\n'
     tmp += f'buildfileGenerators\t= {{"MakefileGenerator"}}\n'
     with open(f'{output_path}/{problem_name}.settings', "w") as file:
@@ -31,10 +31,10 @@ def run_test(problem_name: str, output_path: str, generator_path: str, knowledge
         for i in range(1, 5):
             if f'.exa{i}' in name:
                 subprocess.run(['cp', name, f'{output_path}/{problem_name}.exa{i}'])
-    result = subprocess.run(['java', '-cp', generator_path, 'Main', f'{output_path}/{problem_name}.settings',
+    result = subprocess.run(['java', '-Djava.library.path=scala-isl-utils/libs', '-classpath scala-isl-utils/libs/isl-scala.jar', '-cp', generator_path, 'Main', f'{output_path}/{problem_name}.settings',
                              knowledge_path, platform_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    print(result.stdout.decode('utf-8'))
-    # print(result.stderr.decode('utf-8'))
+    # print(result.stdout.decode('utf-8'))
+    print(result.stderr.decode('utf-8'))
     return result.returncode
 
 
@@ -50,7 +50,7 @@ def main():
         exa_files = []
     else:
         exa_files = exa_files_str.split(';')
-    run_test(problem_name, output_path, generator_path, knowledge_path, platform_path, exa_files, expected_results_path)
+    return run_test(problem_name, output_path, generator_path, knowledge_path, platform_path, exa_files, expected_results_path)
 
 
 if __name__ == "__main__":
