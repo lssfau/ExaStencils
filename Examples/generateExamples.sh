fb4a5afc7f5208a773b7446eba43aa37538a182b
Sebastian <sebastian.kuckuk@fau.de>
2019-08-12 13:28:51 +0200

    add compile lib folder to class path when generating examples

--- Examples/generateExamples.sh
+++ Examples/generateExamples.sh
@@ -5,6 +5,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   platform=lib/mac.platform
 fi
+#platform=lib/windows.platform
 
 source examples.sh
 
@@ -16,7 +17,7 @@ mkdir Debug 2>/dev/null
 for config in $configList; do
   echo generating $config ...
   printf "\033]2;generating $config\007"
-  TIME=$( time java -cp ../Compiler/Compiler.jar Main $config.settings $config.knowledge $platform > ./Debug/${config##*/}_generateResult.txt; exit ${PIPESTATUS[0]}
+  TIME=$( time java -cp ../Compiler/lib:../Compiler/Compiler.jar Main $config.settings $config.knowledge $platform > ./Debug/${config##*/}_generateResult.txt; exit ${PIPESTATUS[0]}
 )
   RET=$?
   echo $TIME
