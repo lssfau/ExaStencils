b438dad5567995fc4695da844718cefd2994474d
Sebastian <sebastian.kuckuk@fau.de>
2019-10-18 09:18:04 +0200

    add license header to all scala files

--- Compiler/src/exastencils/experimental/ir/IR_ResolveGismoFunctions.scala
+++ Compiler/src/exastencils/experimental/ir/IR_ResolveGismoFunctions.scala
@@ -1,3 +1,21 @@
+//=============================================================================
+//
+//  This file is part of the ExaStencils code generation framework. ExaStencils
+//  is free software: you can redistribute it and/or modify it under the terms
+//  of the GNU General Public License as published by the Free Software
+//  Foundation, either version 3 of the License, or (at your option) any later
+//  version.
+//
+//  ExaStencils is distributed in the hope that it will be useful, but WITHOUT
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+//  more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with ExaStencils. If not, see <http://www.gnu.org/licenses/>.
+//
+//=============================================================================
+
 package exastencils.experimental.ir
 
 import exastencils.base.ir.IR_ImplicitConversion._
