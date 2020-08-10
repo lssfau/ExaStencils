cff7ccfbbaa41702681915b0408fb735a0d64e80
Fabian_Böhm <Fabian.Monpreacthyl@gmail.com>
2020-08-10 11:15:48 +0200

    changes to prettyprint

--- Compiler/src/exastencils/baseExt/ir/IR_ComplexExpression.scala
+++ Compiler/src/exastencils/baseExt/ir/IR_ComplexExpression.scala
@@ -4,6 +4,7 @@ import scala.collection.mutable.ListBuffer
 
 import exastencils.base.ir
 import exastencils.base.ir.IR_Addition
+import exastencils.base.ir.IR_BoundedScalar
 import exastencils.base.ir.IR_ComplexDatatype
 import exastencils.base.ir.IR_Datatype
 import exastencils.base.ir.IR_Division
@@ -46,7 +47,7 @@ case class IR_ComplexExpression(real : IR_Expression, op : Boolean, imag : IR_Ex
       case IR_RealDatatype | IR_FloatDatatype | IR_DoubleDatatype | IR_IntegerDatatype if(imag.isInstanceOf[IR_Number]) =>
       case _                                                                              => Logger.error("Need to resolve imag part to floating or integer constant")
     }
-    out << real << (if(op) " + " else " - ") << imag << "i"
+    out << "std::complex<double>(" << real << (if(op) " + " else " - ") << imag << "i)"
   }
 
   /*
@@ -88,6 +89,9 @@ object IR_ComplexOperations {
     c2.signToImag()
     IR_ComplexExpression(c1.real*c2.real - c1.imag * c2.imag, true, c1.real*c2.imag + c1.imag*c2.real)
   }
+  def mult(sc : IR_BoundedScalar, c : IR_ComplexExpression): IR_ComplexExpression = {
+    IR_ComplexExpression(c.real * sc, true, c.imag * sc)
+  }
   def reciprocal(c : IR_ComplexExpression) : IR_ComplexExpression = {
     c.signToImag()
     IR_ComplexExpression(ir.IR_Division(c.real,squareAdd(c)), c.op, IR_Negative(ir.IR_Division(c.imag,squareAdd(c))))
@@ -123,15 +127,17 @@ object IR_ComplexOperations {
 
 object IR_ResolveComplexOps extends DefaultStrategy("Resolve operations with complex expressions") {
     this += new Transformation("resolve", {
-      case add @ IR_Addition(args) if(args.forall(a => a.isInstanceOf[IR_ComplexExpression])) =>
+      case _ @ IR_Addition(args) if(args.forall(a => a.isInstanceOf[IR_ComplexExpression])) =>
           if(args.length != 2) Logger.error("addition with more than 2 ces currently not supported")
         IR_ComplexOperations.add(args(0).asInstanceOf[IR_ComplexExpression], args(1).asInstanceOf[IR_ComplexExpression])
-      case sub @ IR_Subtraction(left,right) if(left.isInstanceOf[IR_ComplexExpression] && right.isInstanceOf[IR_ComplexExpression]) =>
+      case _ @ IR_Subtraction(left,right) if(left.isInstanceOf[IR_ComplexExpression] && right.isInstanceOf[IR_ComplexExpression]) =>
         IR_ComplexOperations.sub(left.asInstanceOf[IR_ComplexExpression],right.asInstanceOf[IR_ComplexExpression])
-      case Mult @ IR_Multiplication(args) if(args.forall(a => a.isInstanceOf[IR_ComplexExpression])) =>
+      case _ @ IR_Multiplication(args) if(args.forall(a => a.isInstanceOf[IR_ComplexExpression])) =>
         if(args.length != 2) Logger.error("multiplication with more than 2 ces currently not supported")
         IR_ComplexOperations.mult(args(0).asInstanceOf[IR_ComplexExpression], args(1).asInstanceOf[IR_ComplexExpression])
-      case Div @ IR_Division(ce1,ce2) if(ce1.isInstanceOf[IR_ComplexExpression] && ce2.isInstanceOf[IR_ComplexExpression]) =>
+      case _ @ IR_Division(ce1,ce2) if(ce1.isInstanceOf[IR_ComplexExpression] && ce2.isInstanceOf[IR_ComplexExpression]) =>
         IR_ComplexOperations.div(ce1.asInstanceOf[IR_ComplexExpression],ce2.asInstanceOf[IR_ComplexExpression])
     })
 }
+
+
