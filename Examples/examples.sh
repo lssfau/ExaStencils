71a93446aa99e001d62df606384f9965c66b66c0
Fabian Böhm <Fabian.boehm@fau.de>
2021-01-13 11:01:39 +0100

    example scripts to master version

--- Examples/examples.sh
+++ Examples/examples.sh
@@ -1,65 +1,66 @@
+
 #!/bin/bash
 
 configList=""
 
-## BiHarmonic
-#
-#configList+="BiHarmonic/2D_FD_BiHarmonic_fromL2 "
-#
-## Stokes
-#
-#configList+="Stokes/1D_FD_Stokes_fromL1 "
-#
-#configList+="Stokes/2D_FD_Stokes_fromL1 "
-#configList+="Stokes/2D_FD_Stokes_fromL2 "
-#configList+="Stokes/2D_FD_Stokes_fromL4 "
-#
-#configList+="Stokes/2D_FV_Stokes_fromL2 "
-#configList+="Stokes/2D_FV_Stokes_fromL4 "
-#
-#configList+="Stokes/3D_FD_Stokes_fromL1 "
-#configList+="Stokes/3D_FD_Stokes_fromL2 "
-#configList+="Stokes/3D_FD_Stokes_fromL4 "
-#
-#configList+="Stokes/3D_FV_Stokes_fromL2 "
-#configList+="Stokes/3D_FV_Stokes_fromL4 "
-#
-#configList+="Stokes/2D_FD_Stokes_Lisa_fromL2 "
-#
-## Poisson
-#
-#configList+="Poisson/1D_FD_Poisson_fromL1 "
-#
-#configList+="Poisson/2D_FD_Poisson_fromL1 "
-#configList+="Poisson/2D_FD_Poisson_fromL2 "
-#configList+="Poisson/2D_FD_Poisson_fromL3 "
-#configList+="Poisson/2D_FD_Poisson_fromL4 "
-#
-#configList+="Poisson/2D_FV_Poisson_fromL2 "
-#configList+="Poisson/2D_FV_Poisson_fromL4 "
-#
-#configList+="Poisson/3D_FD_Poisson_fromL1 "
-#configList+="Poisson/3D_FD_Poisson_fromL2 "
-#configList+="Poisson/3D_FD_Poisson_fromL4 "
-#
-#configList+="Poisson/3D_FV_Poisson_fromL2 "
-#configList+="Poisson/3D_FV_Poisson_fromL4 "
-#
-## OpticalFlow
-#
-#configList+="OpticalFlow/2D_FD_OptFlow_fromL2 "
-#configList+="OpticalFlow/2D_FD_OptFlow_fromL4 "
-#configList+="OpticalFlow/2D_FD_OptFlow_fromL4_Vec "
-#
-#configList+="OpticalFlow/3D_FD_OptFlow_fromL2 "
-#configList+="OpticalFlow/3D_FD_OptFlow_fromL4_Vec "
-#
-## SWE
-#
+# BiHarmonic
+
+configList+="BiHarmonic/2D_FD_BiHarmonic_fromL2 "
+
+# Stokes
+
+configList+="Stokes/1D_FD_Stokes_fromL1 "
+
+configList+="Stokes/2D_FD_Stokes_fromL1 "
+configList+="Stokes/2D_FD_Stokes_fromL2 "
+configList+="Stokes/2D_FD_Stokes_fromL4 "
+
+configList+="Stokes/2D_FV_Stokes_fromL2 "
+configList+="Stokes/2D_FV_Stokes_fromL4 "
+
+configList+="Stokes/3D_FD_Stokes_fromL1 "
+configList+="Stokes/3D_FD_Stokes_fromL2 "
+configList+="Stokes/3D_FD_Stokes_fromL4 "
+
+configList+="Stokes/3D_FV_Stokes_fromL2 "
+configList+="Stokes/3D_FV_Stokes_fromL4 "
+
+configList+="Stokes/2D_FD_Stokes_Lisa_fromL2 "
+
+# Poisson
+
+configList+="Poisson/1D_FD_Poisson_fromL1 "
+
+configList+="Poisson/2D_FD_Poisson_fromL1 "
+configList+="Poisson/2D_FD_Poisson_fromL2 "
+configList+="Poisson/2D_FD_Poisson_fromL3 "
+configList+="Poisson/2D_FD_Poisson_fromL4 "
+
+configList+="Poisson/2D_FV_Poisson_fromL2 "
+configList+="Poisson/2D_FV_Poisson_fromL4 "
+
+configList+="Poisson/3D_FD_Poisson_fromL1 "
+configList+="Poisson/3D_FD_Poisson_fromL2 "
+configList+="Poisson/3D_FD_Poisson_fromL4 "
+
+configList+="Poisson/3D_FV_Poisson_fromL2 "
+configList+="Poisson/3D_FV_Poisson_fromL4 "
+
+# OpticalFlow
+
+configList+="OpticalFlow/2D_FD_OptFlow_fromL2 "
+configList+="OpticalFlow/2D_FD_OptFlow_fromL4 "
+configList+="OpticalFlow/2D_FD_OptFlow_fromL4_Vec "
+
+configList+="OpticalFlow/3D_FD_OptFlow_fromL2 "
+configList+="OpticalFlow/3D_FD_OptFlow_fromL4_Vec "
+
+# SWE
+
 #configList+="SWE/2D_FV_SWE "
-#
-## NavierStokes
-#
+
+# NavierStokes
+
 #configList+="NavierStokes/2D_FV_NavierStokes_Picard "
 #configList+="NavierStokes/2D_FV_NavierStokes_Newton "
 #configList+="NavierStokes/2D_FV_NavierStokes_localPicard "
@@ -75,11 +76,8 @@ configList=""
 #
 #configList+="NavierStokes/3D_FV_NonNewtonian_Picard "
 #configList+="NavierStokes/3D_FV_NonNewtonian_Newton "
-#
-## LinearElasticity
-#
-#configList+="LinearElasticity/2D_FD_LinearElasticity_fromL2 "
 
-# Tensor
+# LinearElasticity
 
-configList+="Tensor/Tensor_test "
+configList+="LinearElasticity/2D_FD_LinearElasticity_fromL2 "
+
