767b206da6257175fa348d86b4ff40fb8f6d1a7a
zy69guqi <richard.angersbach@fau.de>
2021-06-14 10:06:04 +0200

    Introduce CUDA memory transfer elimination knowledge flag.

--- Compiler/src/exastencils/config/Knowledge.scala
+++ Compiler/src/exastencils/config/Knowledge.scala
@@ -486,6 +486,9 @@ object Knowledge {
   var cuda_syncHostForWrites : Boolean = true
   // specifies if fields with (exclusive) write accesses should be synchronized before device kernel executions
   var cuda_syncDeviceForWrites : Boolean = true
+  // ["none"|"both"|"device_to_host"|"host_to_device"] eliminates host <-> device transfers.
+  var cuda_eliminate_memory_transfers = "none"
+  val cuda_memory_transfer_elimination_options = List("none", "both", "device_to_host", "host_to_device")
 
   // default block size in x dimension
   var cuda_blockSize_x : Long = if (dimensionality == 3) 32 else 128
@@ -756,6 +759,8 @@ object Knowledge {
 
     Constraints.condWarn(cuda_enabled && opt_conventionalCSE && !useDblPrecision, "Double precision should be used if CUDA is enabled and CSE should be applied!")
     Constraints.condEnsureValue(useDblPrecision, true, cuda_enabled && opt_conventionalCSE)
+
+    Constraints.condError(!cuda_memory_transfer_elimination_options.contains(cuda_eliminate_memory_transfers), "Invalid value for \"cuda_eliminate_memory_transfers\". Should be one of: " + cuda_memory_transfer_elimination_options.mkString(",") )
 
     Constraints.condWarn(experimental_splitLoopsForAsyncComm && !comm_onlyAxisNeighbors, s"Using asynchronous communication with comm_onlyAxisNeighbors leads to problems with stencils containing diagonal entries")
 
