7717173e72bf9cce789fe5595c4776b552ed8803
zy69guqi <richard.angersbach@fau.de>
2021-06-18 12:13:32 +0200

    Minor fix for printField applied to waLBerla fields.

--- Compiler/src/exastencils/waLBerla/l4/L4_WaLBerlaResolvePrintField.scala
+++ Compiler/src/exastencils/waLBerla/l4/L4_WaLBerlaResolvePrintField.scala
@@ -1,24 +1,22 @@
 package exastencils.waLBerla.l4
 
 import scala.collection.mutable
-import scala.collection.mutable.ListBuffer
 
 import exastencils.datastructures.DefaultStrategy
 import exastencils.datastructures.Transformation
 import exastencils.field.l4.L4_PrintField
+import exastencils.util.l4.L4_StackCollector
 
 object L4_WaLBerlaResolvePrintField extends DefaultStrategy("Resolve printField for waLBerla fields") {
-  var printStmts : ListBuffer[L4_PrintField] = ListBuffer()
+  val collector = new L4_StackCollector
+  this.register(collector)
+  this.onBefore = () => this.resetCollectors()
 
   this += Transformation("Gather print stmts", {
     case print @ L4_PrintField(_, fAcc, _, _, _, _) if L4_WaLBerlaFieldCollection.contains(fAcc) =>
-      printStmts += print
-      print
-  })
-
-  this += Transformation("Wrap around loopOverBlock", {
-    case print : L4_PrintField if printStmts.contains(print) =>
-      printStmts -= print
-      L4_WaLBerlaLoopOverBlocks(mutable.ListBuffer(print), reduction = None)
+      if (!collector.stack.exists(_.isInstanceOf[L4_WaLBerlaLoopOverBlocks]))
+        L4_WaLBerlaLoopOverBlocks(mutable.ListBuffer(print), reduction = None)
+      else
+        print
   })
 }
