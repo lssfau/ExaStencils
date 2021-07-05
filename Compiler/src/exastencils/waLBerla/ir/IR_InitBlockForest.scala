4c912ddbb77ab844344e7b6109f2b6e68202d96c
Richard Angersbach <richard.angersbach@fau.de>
2021-07-05 10:47:19 +0200

    Fixes for IR_InitBlockForest.

--- Compiler/src/exastencils/waLBerla/ir/IR_InitBlockForest.scala
+++ Compiler/src/exastencils/waLBerla/ir/IR_InitBlockForest.scala
@@ -21,29 +21,33 @@ case class IR_InitBlockForest() extends IR_WaLBerlaFuturePlainFunction {
   def domainBounds = domain.asInstanceOf[IR_DomainFromAABB].aabb
   def datatype = IR_SharedPointerDatatype(WB_StructuredBlockStorage)
 
+  def someWaLBerlaField = if (IR_WaLBerlaFieldCollection.objects.nonEmpty) Some(IR_WaLBerlaFieldCollection.objects.maxBy(_.level)) else None
+
   val numDims  = Knowledge.dimensionality
-  val level = Knowledge.maxLevel
+  val level = if (someWaLBerlaField.isDefined) someWaLBerlaField.get.level else -1
 
   def toUnsignedInt(value : Int) = IR_Cast(IR_SpecialDatatype("uint_t"), value)
 
-  def someWaLBerlaField = IR_WaLBerlaFieldCollection.objects.collectFirst { case f : IR_WaLBerlaField if f.level == level => f }.get
-
-  def numCellsTotal(d : Int) = if (d < numDims) someWaLBerlaField.layout.layoutsPerDim(d).numInnerLayers else 1
-  def cellWidth(d : Int) = (domainBounds.upper(d) - domainBounds.lower(d)) / numCellsTotal(d)
+  def numCells(d : Int) = if (someWaLBerlaField.isDefined && d < numDims) someWaLBerlaField.get.layout.layoutsPerDim(d).numInnerLayers else 1 << level
+  def numCellsTotal(d : Int) = if (d < numDims) Knowledge.domain_rect_numFragsTotalAsVec(d) * Knowledge.domain_fragmentLengthAsVec(d) * numCells(d) else 1
+  def cellWidth(d : Int) = if (d < numDims) (domainBounds.upper(d) - domainBounds.lower(d)) / numCellsTotal(d) else 0
 
   val wbBlocks = List(Knowledge.domain_rect_numFragsTotal_x, Knowledge.domain_rect_numFragsTotal_y, Knowledge.domain_rect_numFragsTotal_z).map(toUnsignedInt)
   val numProcesses = List(Knowledge.domain_rect_numBlocks_x, Knowledge.domain_rect_numBlocks_y, Knowledge.domain_rect_numBlocks_z).map(toUnsignedInt)
   val periodicity = List(Knowledge.domain_rect_periodic_x, Knowledge.domain_rect_periodic_y, Knowledge.domain_rect_periodic_z)
 
-  // assumes all top level fields have the same number of cells
-  if (IR_WaLBerlaFieldCollection.objects.filter(_.level == level).forall(f => (0 until numDims).map( d => f.layout.layoutsPerDim(d).numInnerLayers != numCellsTotal(d)).reduce(_ || _)))
-    Logger.error("IR_InitBlockForest: Top-level waLBerla fields must have the same size.")
-
-  // step size in each dimension must be identical
-  if ((0 until numDims).map(cellWidth).distinct.length != 1)
-    Logger.error("Trying to call IR_InitBlockForest with different step sizes.")
-
   override def generateFct() : IR_WaLBerlaPlainFunction = {
+    // error checks
+    if (IR_WaLBerlaFieldCollection.objects.nonEmpty) {
+      // assumes all top level fields have the same number of cells
+      if (IR_WaLBerlaFieldCollection.objects.filter(_.level == level).forall(f => (0 until numDims).map(d => f.layout.layoutsPerDim(d).numInnerLayers != numCells(d)).reduce(_ || _)))
+        Logger.error("IR_InitBlockForest: Top-level waLBerla fields must have the same size.")
+
+      // step size in each dimension must be identical
+      if ((0 until numDims).map(cellWidth).distinct.length != 1)
+        Logger.error("Trying to call IR_InitBlockForest with different step sizes.")
+    }
+
     // add deps
     IR_WaLBerlaCollection.get.addExternalDependency("blockforest/Initialization.h")
 
