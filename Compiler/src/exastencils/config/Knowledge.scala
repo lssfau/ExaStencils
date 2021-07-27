f56a92165c5aca1159f3ba52c8c19e987e5c27b9
Richard Angersbach <richard.angersbach@fau.de>
2021-07-27 13:30:14 +0200

    Add TODOs and minor refactoring for VisIt interface.

--- Compiler/src/exastencils/config/Knowledge.scala
+++ Compiler/src/exastencils/config/Knowledge.scala
@@ -655,9 +655,6 @@ object Knowledge {
 
   var experimental_grid_randomMaxOffset : Double = 0.1
 
-  /// student project - Richard / visit
-
-  // TODO
   var experimental_visit_enable : Boolean = false
 
   /// === constraints and resolutions ===
