e286f61549e41456f20ecb2b27da8356eab198f5
zy69guqi <richard.angersbach@fau.de>
2021-12-17 13:56:06 +0100

    Test commit: Change operand order for NEON bit select instruction.

--- Compiler/src/exastencils/simd/SIMD_Blend.scala
+++ Compiler/src/exastencils/simd/SIMD_Blend.scala
@@ -17,7 +17,7 @@ case class SIMD_Blendv(var a : IR_Expression, var b : IR_Expression, var mask : 
       case "SSE3"         => out << "_mm_or_p" << prec << "( _mm_and_p" << prec << "(" << b << "," << mask << "), _mm_andnot_p" << prec << "(" << mask << "," << a << ") )"
       case "AVX" | "AVX2" => out << "_mm256_blendv_p" << prec << '(' << a << "," << b << "," << mask << ")"
       case "AVX512"       => out << "_mm512_mask_blend_p" << prec << '(' << mask << "," << a << "," << b << ")"
-      case "NEON"         => out <<  "vbslq_f" << precBits << "(" << mask << ", " << a << ", " << b << ")"
+      case "NEON"         => out <<  "vbslq_f" << precBits << "(" << mask << ", " << b << ", " << a << ")"
       // TODO
       case "IMCI"         => new VectorizationException("SIMD_Blendv: Currently unsupported for " + Platform.simd_instructionSet)
       case "QPX"          => new VectorizationException("SIMD_Blendv: Currently unsupported for " + Platform.simd_instructionSet)
