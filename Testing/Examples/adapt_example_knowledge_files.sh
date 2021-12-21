3d47d31522b36200c0aaf16d5c5733ad768d495b
zy69guqi <richard.angersbach@fau.de>
2021-12-21 14:20:58 +0100

    Remove duplicate newlines at generated CI example knowledge files.

--- Testing/Examples/adapt_example_knowledge_files.sh
+++ Testing/Examples/adapt_example_knowledge_files.sh
@@ -6,14 +6,15 @@ for i in `find ../../Examples/ -type f -name "*.knowledge"`; do
     base=$(basename $i)
     file_name="$dir_name/$base"
 
-    # copy
+    # copy over
     mkdir -p $dir_name
     cp $i $file_name
 
     # modify copy
-    sed -i '/library_CImg/d' $file_name
+    sed -i '/library_CImg/d' $file_name # disable CImg flag
+    sed -i 's#../lib#../../../Examples/lib#g' $file_name # adapt path for import
+    sed -i -e :a -e '/^\n*$/{$d;N;ba' -e '}' $file_name # delete empty lines at end
     echo '' >> $file_name
-    echo 'testing_enabled = true' >> $file_name
-    sed -i 's#../lib#../../../Examples/lib#g' $file_name
+    echo 'testing_enabled = true' >> $file_name # enable testing mode
 
 done
