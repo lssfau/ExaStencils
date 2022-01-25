8bba3e45286da671b2a6e8d872856ec5bc042e05
Sebastian Kuckuk <sebastian.kuckuk@fau.de>
2022-01-25 14:21:43 +0100

    check for additional errors that are not reported by default in CUDA_CheckError

--- Compiler/src/exastencils/parallelization/api/cuda/CUDA_Error.scala
+++ Compiler/src/exastencils/parallelization/api/cuda/CUDA_Error.scala
@@ -42,12 +42,22 @@ case class CUDA_CheckError(var exp : IR_Expression) extends CUDA_HostStatement w
   override def expand() : Output[StatementList] = {
     val statusName = s"cudaStatus_$counter"
     counter += 1
+
     def status = IR_VariableAccess(statusName, IR_SpecialDatatype("cudaError_t"))
+
+    def print = IR_RawPrint("\"CUDA error in file (\"", "__FILE__", "\"), line (\"", "__LINE__", "\"): \"", status,
+      "\" -> \"", IR_FunctionCall(IR_ExternalFunctionReference("cudaGetErrorString"), status), "std::endl")
 
     ListBuffer(
       IR_VariableDeclaration(status, exp),
       IR_IfCondition("cudaSuccess" Neq status,
-        IR_RawPrint("\"CUDA error in file (\"", "__FILE__", "\"), line (\"", "__LINE__", "\"): \"", status,
-          "\" -> \"", IR_FunctionCall("cudaGetErrorString", status), "std::endl")))
+        print,
+        ListBuffer(
+          IR_Assignment(status, IR_FunctionCall(IR_ExternalFunctionReference("cudaGetLastError"))),
+          IR_IfCondition("cudaSuccess" Neq status,
+            ListBuffer[IR_Statement](
+              print,
+              IR_FunctionCall(IR_ExternalFunctionReference("exit"), 1))
+          ))))
   }
 }
