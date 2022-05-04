d9fbf5770a0373a54357854c5340a4eedf4ed191
zy69guqi <richard.angersbach@fau.de>
2022-05-04 18:05:13 +0200

    Add printJSON function.

--- Compiler/src/exastencils/app/ir/IR_LayerHandler.scala
+++ Compiler/src/exastencils/app/ir/IR_LayerHandler.scala
@@ -133,6 +133,7 @@ object IR_DefaultLayerHandler extends IR_LayerHandler {
     IR_ResolveStationFunctions.apply()
     IR_ResolveCImgFunctions.apply()
     IR_ResolveCharacteristicsFunctions.apply()
+    IR_ResolveJSONFunctions.apply()
     IR_ResolveBenchmarkFunctions.apply()
     IR_ResolveGismoFunctions.apply()
     IR_ResolveVtkPrinters.apply()
