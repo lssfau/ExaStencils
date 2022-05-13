e2ba23dcabfea2a9cf9057f90b0ee588f3095346
zy69guqi <richard.angersbach@fau.de>
2022-05-13 17:16:17 +0200

    Use os.system for loading modules [skip ci]

--- Benchmark/module_tree_run.py
+++ Benchmark/module_tree_run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import subprocess
+import os
 from run_benchmark import check_err
 
 
@@ -8,23 +9,29 @@ from run_benchmark import check_err
 
 @check_err
 def load_mpi_module():
-    subprocess.run(["module", "load", "openmpi/4.1.1-gcc"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    os.system("module load openmpi/4.1.1-gcc")
     return subprocess.run(["mpirun", "--version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 
 @check_err
 def load_likwid_module():
-    subprocess.run(["module", "load", "likwid/5.2.1"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    os.system("module load likwid/5.2.1")
     return subprocess.run(["likwid-features", "--version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 
 @check_err
+def check_nvcc():
+    return subprocess.run(["nvcc", "--version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+@check_err
+def check_nvidia_smi():
+    return subprocess.run(["nvidia-smi"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+@check_err
 def load_cuda_module():
-    subprocess.run(["module", "load", "cuda/11.4"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    result = subprocess.run(["nvcc", "--version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    if not result.returncode == 0:
-        print(result.stderr.decode('utf-8'))
-        return result.returncode
-    result = subprocess.run(["nvidia-smi"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    subprocess.run(["updatedb"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    return result
+    os.system("module load cuda/11.4")
+    check_nvcc()
+    check_nvidia_smi()
+    return subprocess.run(["updatedb"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
