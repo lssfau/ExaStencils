38aaf7ec2a6aa6ea8f18efefab8c8e19a030b014
zy69guqi <richard.angersbach@fau.de>
2022-05-13 13:12:32 +0200

    Adapt job script generation [skip ci]

--- Benchmark/generate_jobs_for_hosts.sh
+++ Benchmark/generate_jobs_for_hosts.sh
@@ -1,18 +1,15 @@
 #!/bin/bash -l
 
-if [ $# -ne 2 ]
-  then
-    echo "Wrong number of arguments. Should be ./<script_name>.sh <EXA_PROBLEM_NAME> <EXA_PROBLEM_PATH>"
+if [ $# -ne 2 ]; then
+  echo "Wrong number of arguments. Should be ./<script_name>.sh <EXA_PROBLEM_NAME> <EXA_PROBLEM_PATH>"
 fi
 
-# Get list of hosts in the testcluster (requires NO_SLURM_SUBMIT=1)
-HOSTLIST=$(sinfo -h --partition=work -o "%n" | grep rome1 ) # TODO remove grep
+HOSTLIST=$(sinfo -h --partition=work -o "%n" | grep rome1) # TODO remove grep
 for HOST in ${HOSTLIST}; do
-  cat << EOF
-benchmark-$1-$HOST:
-  stage: benchmark
-  extends: .benchmark_template
-  variables:
-    EXA_PROBLEM_PATH: "$2"
-  EOF
+  echo "benchmark-$1-$HOST:"
+  echo -e "\tstage: benchmark"
+  echo -e "\textends: .benchmark_template"
+  echo -e "\tvariables:"
+  echo -e "\t\tEXA_PROBLEM_PATH: "$2""
+  echo
 done
