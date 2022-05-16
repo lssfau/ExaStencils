aa4a5b50f21341f5fce85d62ff782a096249533a
zy69guqi <richard.angersbach@fau.de>
2022-05-16 23:44:06 +0200

    Rename stages in cb pipeline [skip ci]

--- Benchmark/generate_bench_jobs_for_hosts.sh
+++ Benchmark/generate_bench_jobs_for_hosts.sh
@@ -7,13 +7,13 @@ fi
 HOSTLIST=$(sinfo -h --partition=work -o "%n")
 for HOST in ${HOSTLIST}; do
   echo "stages:"
-  echo "    - benchmark"
+  echo "    - bench_pipe"
   echo ""
   echo "$(<.benchmark_templates.yml)"
   echo ""
   echo "benchmark-$1-$HOST:"
-  echo -e "    stage: benchmark"
-  echo -e "    extends: .benchmark_template"
+  echo -e "    stage: bench_pipe"
+  echo -e "    extends: .bench_pipe_template"
   echo -e "    variables:"
   echo -e "        EXA_PROBLEM_PATH: "$2""
   echo
--- Benchmark/generate_pf_jobs_for_hosts.sh
+++ Benchmark/generate_pf_jobs_for_hosts.sh
@@ -3,12 +3,12 @@
 HOSTLIST=$(sinfo -h --partition=work -o "%n")
 for HOST in ${HOSTLIST}; do
   echo "stages:"
-  echo "    - pf_gen"
+  echo "    - pf_pipe"
   echo ""
   echo "$(<.benchmark_templates.yml)"
   echo ""
   echo "pf-gen-$HOST:"
-  echo -e "    stage: pf_gen"
+  echo -e "    stage: pf_pipe"
   echo -e "    extends: .pfgen_template"
   echo -e "    variables:"
   echo -e "        HOSTNAME: \"$HOST\""
