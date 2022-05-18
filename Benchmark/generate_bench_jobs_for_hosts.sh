35ce65ef32ed16fc069b7e225e911fcd1613ea1d
zy69guqi <richard.angersbach@fau.de>
2022-05-18 18:14:19 +0200

    Filter nodes without python submodules from pipeline execution [skip ci]

--- Benchmark/generate_bench_jobs_for_hosts.sh
+++ Benchmark/generate_bench_jobs_for_hosts.sh
@@ -4,17 +4,19 @@ if [ $# -ne 2 ]; then
   echo "Wrong number of arguments. Should be ./<script_name>.sh <EXA_PROBLEM_NAME> <EXA_PROBLEM_PATH>"
 fi
 
-HOSTLIST=$(sinfo -t idle -h --partition=work -o "%n" | sed '/aurora1/d')
+HOSTLIST=$(sinfo -t idle -h --partition=work -o "%n")
 for HOST in ${HOSTLIST}; do
-  echo "stages:"
-  echo "    - bench_pipe"
-  echo ""
-  echo "$(<.benchmark_templates.yml)"
-  echo ""
-  echo "benchmark-$1-$HOST:"
-  echo -e "    stage: bench_pipe"
-  echo -e "    extends: .bench_pipe_template"
-  echo -e "    variables:"
-  echo -e "        EXA_PROBLEM_PATH: "$2""
-  echo
+  if [[ "$HOST" != "aurora1" || "$HOST" != "warmup" ]]; then
+    echo "stages:"
+    echo "    - bench_pipe"
+    echo ""
+    echo "$(<.benchmark_templates.yml)"
+    echo ""
+    echo "benchmark-$1-$HOST:"
+    echo -e "    stage: bench_pipe"
+    echo -e "    extends: .bench_pipe_template"
+    echo -e "    variables:"
+    echo -e "        EXA_PROBLEM_PATH: "$2""
+    echo
+  fi
 done
--- Benchmark/generate_pf_jobs_for_hosts.sh
+++ Benchmark/generate_pf_jobs_for_hosts.sh
@@ -1,16 +1,18 @@
 #!/bin/bash -l
 
-HOSTLIST=$(sinfo -t idle -h --partition=work -o "%n" | sed '/aurora1/d')
+HOSTLIST=$(sinfo -t idle -h --partition=work -o "%n")
 for HOST in ${HOSTLIST}; do
-  echo "stages:"
-  echo "    - pf_pipe"
-  echo ""
-  echo "$(<.benchmark_templates.yml)"
-  echo ""
-  echo "pf-gen-$HOST:"
-  echo -e "    stage: pf_pipe"
-  echo -e "    extends: .pfgen_template"
-  echo -e "    variables:"
-  echo -e "        SLURM_NODELIST: \"$HOST\""
-  echo
+  if [[ "$HOST" != "aurora1" || "$HOST" != "warmup" ]]; then
+    echo "stages:"
+    echo "    - pf_pipe"
+    echo ""
+    echo "$(<.benchmark_templates.yml)"
+    echo ""
+    echo "pf-gen-$HOST:"
+    echo -e "    stage: pf_pipe"
+    echo -e "    extends: .pfgen_template"
+    echo -e "    variables:"
+    echo -e "        SLURM_NODELIST: \"$HOST\""
+    echo
+  fi
 done
