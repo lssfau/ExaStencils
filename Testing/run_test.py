c3d5465fc4e79fcaece2e524bdea9c1950a601f9
zy69guqi <richard.angersbach@fau.de>
2022-06-01 18:26:58 +0200

    Use new python utility in run_test.py

--- Testing/run_test.py
+++ Testing/run_test.py
@@ -1,28 +1,11 @@
+#!/usr/bin/env python3
+
+import argparse
 import sys
-import os
-import subprocess
 
-
-def generate_settings_file(problem_name: str, output_path: str, exa_file_names: [str]):
-    tmp = f'user\t= "Guest"\n\n'
-
-    tmp += f'basePathPrefix\t= "{output_path}"\n\n'
-    for name in exa_file_names:
-        for i in range(1, 5):
-            if f'.exa{i}' in name:
-                tmp += f'l{i}file\t = "{problem_name}.exa{i}"\n'
-
-    tmp += f'debugL1File\t= "Debug/{problem_name}_debug.exa1"\n'
-    tmp += f'debugL2File\t= "Debug/{problem_name}_debug.exa2"\n'
-    tmp += f'debugL3File\t= "Debug/{problem_name}_debug.exa3"\n'
-    tmp += f'debugL4File\t= "Debug/{problem_name}_debug.exa4"\n\n'
-    tmp += f'htmlLogFile\t= "Debug/{problem_name}_log.html"\n\n'
-    tmp += f'outputPath\t= "generated/{problem_name}"\n\n'
-    tmp += f'produceHtmlLog\t= true\n'
-    tmp += f'timeStrategies\t= true\n\n'
-    tmp += f'buildfileGenerators\t= {{"MakefileGenerator"}}\n'
-    with open(f'{output_path}/{problem_name}.settings', "w") as file:
-        print(tmp, file=file)
+sys.path.append("../Utilities")
+from run_exastencils import *
+from generation_helpers import *
 
 
 def check_results(result_str: str, expected_results_path: str):
@@ -58,64 +41,45 @@ def check_results(result_str: str, expected_results_path: str):
     return True
 
 
-def run_test(generator_path: str, problem_name: str, knowledge_path: str, exa_file_names: [str],
-             expected_results_path: str, nprocs: int, nthreads: int, platform_path: str, output_path: str):
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
-    generate_settings_file(problem_name, output_path, exa_file_names)
-    for name in exa_file_names:
-        for i in range(1, 5):
-            if f'.exa{i}' in name:
-                subprocess.run(['cp', name, f'{output_path}/{problem_name}.exa{i}'])
-    result = subprocess.run(['java', '-cp', generator_path, 'Main', f'{output_path}/{problem_name}.settings',
-                             knowledge_path, platform_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    if not result.returncode == 0:
-        print(result.stderr.decode('utf-8'))
-        return result.returncode
-    # Fix for strange behavior of PolyExpl tests
-    if "PolyExpl" in problem_name:
-        problem_name += "00000"
-    result = subprocess.run(['make', '-j', '-s', '-C', f'{output_path}/generated/{problem_name}'],
-                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    if not result.returncode == 0:
-        print(result.stderr.decode('utf-8'))
-        return result.returncode
-    result = subprocess.run([f'mpirun', '--allow-run-as-root', '--oversubscribe', '--mca', 'btl_base_warn_component_unused', '0',
-                             f'-np', f'{nprocs}',
-                             f'{output_path}/generated/{problem_name}/exastencils'],
-                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    if not result.returncode == 0:
-        print(result.stderr.decode('utf-8'))
-        return result.returncode
-    elif expected_results_path:
+def main():
+    # parse args
+    parser = argparse.ArgumentParser(description='Generate Benchmark from ExaSlang and run')
+    parser.add_argument('problem_name', type=str, help='Name of the problem. Used as directory name for the generated code')
+    parser.add_argument('exa_problem_path', type=str, help='Path to the ExaSlang problem specification')
+    parser.add_argument('knowledge_file', type=str, help='Knowledge path assumed to be in \"exa_problem_path\"')
+    parser.add_argument('exaslang_files', type=str, help='Comma-separated ExaSlang paths assumed to be in \"exa_problem_path\"')
+    parser.add_argument('expected_results', type=str, help='Path to file with expected results. Assumed to be in \"exa_problem_path\"')
+    parser.add_argument('platform_path', type=str, help='Path to the platform description')
+    parser.add_argument('output_path', type=str, help='Path to output directory')
+    parser.add_argument('--overwrite_settings', type=bool, default=True, help='Overwrite settings files')
+    args = parser.parse_args()
+
+    # print arguments
+    print(f"Executing: python3 run_test.py {' '.join(f'{k}={v}' for k, v in vars(args).items())}")
+
+    # set run options
+    ctx = RunContext(args)
+
+    # generate target code
+    generate_code(ctx)
+
+    # compile target code
+    compile_code(ctx)
+
+    # parse knowledge and platform file
+    config = ConfigFromKnowledge(ctx.problem_name, ctx.knowledge_path, ctx.platform_path)
+
+    # run target code
+    result = run_code(ctx, config)
+
+    # compare with expecred results
+    if args.expected_results:
         result_str = result.stdout.decode('utf-8')
-        if check_results(result_str, expected_results_path) is True:
-            print(f"Test for problem \"{problem_name}\" finished successfully.")
+        if check_results(result_str, get_file_in_problem_path(ctx.exa_problem_path, args.expected_results)) is True:
+            print(f"Test for problem \"{ctx.problem_name}\" finished successfully.")
             return result.returncode
         else:
             return -1
-    else:
-        print(f"Test for problem \"{problem_name}\" finished successfully.")
-        return result.returncode
-
-
-def main():
-    generator_path = sys.argv[1]
-    problem_name = sys.argv[2]
-    knowledge_path = sys.argv[3]
-    exa_files_str = sys.argv[4]
-    expected_results_path = sys.argv[5]
-    nprocs = int(sys.argv[6])
-    nthreads = int(sys.argv[7])
-    platform_path = sys.argv[8]
-    output_path = sys.argv[9]
-    if exa_files_str == '*':
-        exa_files = []
-    else:
-        exa_files = exa_files_str.split(';')
-    retval = run_test(generator_path, problem_name, knowledge_path, exa_files, expected_results_path,
-                    nprocs, nthreads, platform_path, output_path)
-    sys.exit(retval)
 
 
 if __name__ == "__main__":
