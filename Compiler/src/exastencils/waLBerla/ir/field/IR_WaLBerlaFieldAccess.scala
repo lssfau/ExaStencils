efc7c3be41502ff298e787c4c0a064a87b74fbf3
zy69guqi <richard.angersbach@fau.de>
2022-06-02 14:34:24 +0200

    Revert "Remove redundant IR_WaLBerlaFieldAccessLike"
    This reverts commit 4d96f9ab

--- Compiler/src/exastencils/waLBerla/ir/field/IR_WaLBerlaFieldAccess.scala
+++ Compiler/src/exastencils/waLBerla/ir/field/IR_WaLBerlaFieldAccess.scala
@@ -14,6 +14,13 @@ import exastencils.optimization.ir.IR_GeneralSimplify
 import exastencils.prettyprinting.PpStream
 import exastencils.prettyprinting.PrettyPrintable
 
+/// IR_WaLBerlaFieldAccessLike
+
+trait IR_WaLBerlaFieldAccessLike extends IR_FieldLikeAccess {
+  def field : IR_WaLBerlaField
+  override def target : IR_WaLBerlaField = field
+}
+
 object IR_WaLBerlaFieldAccess {
   def apply(field : IR_WaLBerlaField, slot : IR_Expression, index : IR_ExpressionIndex) : IR_WaLBerlaFieldAccess =
     new IR_WaLBerlaFieldAccess(field, slot, IR_LoopOverFragments.defIt, index)
@@ -27,7 +34,7 @@ case class IR_WaLBerlaFieldAccess(
     var offset : Option[IR_ConstIndex] = None,
     var frozen : Boolean = false,
     var matIndex : Option[IR_MatIndex] = None
-) extends IR_FieldLikeAccess with PrettyPrintable {
+) extends IR_WaLBerlaFieldAccessLike with PrettyPrintable {
 
   override def datatype = field.layout.datatype
 
