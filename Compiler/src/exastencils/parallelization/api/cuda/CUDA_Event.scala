b140e9ba3575ef9033f7cc61d84ae77cbb4a8000
zy69guqi <richard.angersbach@fau.de>
2022-06-14 14:00:34 +0200

    Add initial version of CUDA_Event IV

---
+++ Compiler/src/exastencils/parallelization/api/cuda/CUDA_Event.scala
@@ -0,0 +1,43 @@
+package exastencils.parallelization.api.cuda
+
+import exastencils.base.ir._
+import exastencils.base.ir.IR_ImplicitConversion._
+import exastencils.baseExt.ir._
+import exastencils.config.Knowledge
+import exastencils.field.ir.IR_Field
+
+/// CUDA_Event
+
+abstract class CUDA_Event(
+    canBePerFragment : Boolean,
+    canBePerDomain : Boolean,
+    canBePerField : Boolean,
+    canBePerLevel : Boolean,
+    canBePerNeighbor : Boolean
+) extends IR_InternalVariable(canBePerFragment, canBePerDomain, canBePerField, canBePerLevel, canBePerNeighbor) {
+
+  override def getCtor() : Option[IR_Statement] = {
+    Some(wrapInLoops(
+      IR_FunctionCall(IR_ExternalFunctionReference("cudaEventCreateWithFlags"),
+        IR_AddressOf(
+          resolveAccess(resolveName(), IR_LoopOverFragments.defIt, IR_LoopOverDomains.defIt, IR_LoopOverFields.defIt, IR_LoopOverLevels.defIt, IR_LoopOverNeighbors.defIt)),
+        IR_VariableAccess("cudaEventDisableTiming", IR_UnknownDatatype))))
+  }
+
+  override def getDtor() : Option[IR_Statement] = {
+    Some(wrapInLoops(
+      IR_FunctionCall(IR_ExternalFunctionReference("cudaEventDestroy"),
+        resolveAccess(resolveName(), IR_LoopOverFragments.defIt, IR_LoopOverDomains.defIt, IR_LoopOverFields.defIt, IR_LoopOverLevels.defIt, IR_LoopOverNeighbors.defIt))))
+  }
+}
+
+case class CUDA_PendingStreamTransfers(
+    var field : IR_Field,
+    var fragmentIdx : IR_Expression = IR_LoopOverFragments.defIt)
+  extends CUDA_Event(true, false, true, false, false) {
+
+  override def resolveName() : String = "pendingStreamTransferEvent" +
+    resolvePostfix(fragmentIdx.prettyprint, "", if (Knowledge.data_useFieldNamesAsIdx) field.name else field.index.toString, "", "")
+
+  override def resolveDatatype() : IR_Datatype = IR_SpecialDatatype("cudaEvent_t")
+}
