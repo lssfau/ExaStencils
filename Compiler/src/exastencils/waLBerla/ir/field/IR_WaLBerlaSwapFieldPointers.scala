a56d6f9a87686bfaa17465a4d4d54f4b4a4b1bf6
zy69guqi <richard.angersbach@fau.de>
2022-10-12 14:17:52 +0200

    Reimplement walberla's "swapDataPointers" node as expandable

--- Compiler/src/exastencils/waLBerla/ir/field/IR_WaLBerlaSwapFieldPointers.scala
+++ Compiler/src/exastencils/waLBerla/ir/field/IR_WaLBerlaSwapFieldPointers.scala
@@ -1,22 +1,18 @@
 package exastencils.waLBerla.ir.field
 
-import exastencils.base.ir.IR_Statement
+import exastencils.base.ir._
 import exastencils.baseExt.ir.IR_LoopOverFragments
 import exastencils.logger.Logger
-import exastencils.prettyprinting.PpStream
-import exastencils.prettyprinting.PrettyPrintable
 
 case class IR_WaLBerlaSwapFieldPointers(
     var src : IR_WaLBerlaFieldAccess,
     var dst : IR_WaLBerlaFieldAccess,
-) extends IR_Statement with PrettyPrintable {
+) extends IR_Statement with IR_Expandable {
 
   private def toWbData(wbfAcc : IR_WaLBerlaFieldAccess) = IR_IV_WaLBerlaFieldData(wbfAcc.field, wbfAcc.slot, IR_LoopOverFragments.defIt)
 
   if (IR_WaLBerlaFieldCollection.getByIdentifier(src.name, src.level).isEmpty || IR_WaLBerlaFieldCollection.getByIdentifier(dst.name, dst.level).isEmpty)
     Logger.error("\"IR_WaLBerlaSwapFieldPointers\": Both fields must be waLBerla fields")
 
-  override def prettyprint(out : PpStream) : Unit = {
-    out << toWbData(src) << "->swapDataPointers(" << toWbData(dst) << ");"
-  }
+  override def expand() = IR_MemberFunctionCallArrow(toWbData(src), "swapDataPointers", toWbData(dst))
 }
