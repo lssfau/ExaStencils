f543fbc913746f845c3b7b7b931bc5c7359e9672
zy69guqi <richard.angersbach@fau.de>
2022-10-20 15:14:01 +0200

    Try generating CB child pipelines for benchmark stages

---
+++ Benchmark/generate_pipeline_stages.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -l
+
+if [ $# -ne 4 ]; then
+  echo "Wrong number of arguments. Should be ./<script_name>.sh <EXA_PROBLEM_NAME> <EXA_PROBLEM_PATH> <EXASLANG_FILES> <KNOWLEDGE_FILE>"
+fi
+
+PROBLEM_NAME=$1
+PROBLEM_PATH=$2
+EXASLANG=$3
+KNOWLEDGE=$4
+
+#######################################
+##                                   ##
+##       Benchmark Generation        ##
+##                                   ##
+#######################################
+
+echo -e "bench_gen:$PROBLEM_NAME:"
+echo -e "    extends: .gen_bench_job_template"
+echo -e "    variables:"
+echo -e "        EXA_PROBLEM_NAME: \"$PROBLEM_NAME\""
+echo -e "        EXA_PROBLEM_PATH: \"$PROBLEM_PATH\""
+echo -e "        EXASLANG_FILES: \"$EXASLANG\""
+echo -e "        KNOWLEDGE_FILE: \"$KNOWLEDGE\""
+echo
+
+#######################################
+##                                   ##
+##       Benchmark Execution         ##
+##                                   ##
+#######################################
+
+echo -e "bench_pipe:$PROBLEM_NAME:"
+echo -e "    extends: .pipe_bench_job_template"
+echo -e "    variables:"
+echo -e "        EXA_PROBLEM_NAME: \"$PROBLEM_NAME\""
+echo -e "        EXA_PROBLEM_PATH: \"$PROBLEM_PATH\""
+echo -e "        EXASLANG_FILES: \"$EXASLANG\""
+echo -e "        KNOWLEDGE_FILE: \"$KNOWLEDGE\""
+echo
+
+echo -e "bench_pipe:${PROBLEM_NAME}_CUDA:"
+echo -e "    extends: .cuda_job_template"
+echo -e "    variables:"
+echo -e "        SLURM_NODELIST: \"medusa\""
+echo -e "        EXA_PROBLEM_NAME: \"$PROBLEM_NAME\""
+echo -e "        EXA_PROBLEM_PATH: \"$PROBLEM_PATH\""
+echo -e "        EXASLANG_FILES: \"$EXASLANG\""
+echo -e "        KNOWLEDGE_FILE: \"$KNOWLEDGE\""
+echo -e "        PLATFORM_FILE: \"Platform/medusa_QuadroRTX6000.platform\""
+echo
