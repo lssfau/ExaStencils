7fbef287e205a790531b95d7a8dd5bfd190b2a3a
zy69guqi <richard.angersbach@fau.de>
2022-10-20 19:03:49 +0200

    Omit stage generation

--- Benchmark/generate_cb_yml_stages.sh
+++ Benchmark/generate_cb_yml_stages.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -l
 
+# utility script to generate pipeline stages for CB
+
 if [ $# -ne 4 ]; then
   echo "Wrong number of arguments. Should be ./<script_name>.sh <EXA_PROBLEM_NAME> <EXA_PROBLEM_PATH> <EXASLANG_FILES> <KNOWLEDGE_FILE>"
 fi
@@ -9,22 +11,6 @@ PROBLEM_PATH=$2
 EXASLANG=$3
 KNOWLEDGE=$4
 
-echo "stages:"
-echo "    - build"
-echo "    - stage_gen"
-echo "    - stage_pipe"
-echo "    - bench_gen"
-echo "    - bench_pipe"
-echo ""
-echo "$(<.benchmark_templates.yml)"
-echo ""
-
-#######################################
-##                                   ##
-##       Benchmark Generation        ##
-##                                   ##
-#######################################
-
 echo -e "bench_gen:$PROBLEM_NAME:"
 echo -e "    stage: bench_gen"
 echo -e "    extends: .gen_bench_job_template"
@@ -33,16 +19,7 @@ echo -e "        EXA_PROBLEM_NAME: \"$PROBLEM_NAME\""
 echo -e "        EXA_PROBLEM_PATH: \"$PROBLEM_PATH\""
 echo -e "        EXASLANG_FILES: \"$EXASLANG\""
 echo -e "        KNOWLEDGE_FILE: \"$KNOWLEDGE\""
-echo -e "    needs:"
-echo -e "        - pipeline: \"$PARENT_PARENT_PIPELINE_ID\""
-echo -e "          job: stage_gen:$PROBLEM_NAME"
 echo
-
-#######################################
-##                                   ##
-##       Benchmark Execution         ##
-##                                   ##
-#######################################
 
 echo -e "bench_pipe:$PROBLEM_NAME:"
 echo -e "    stage: bench_pipe"
@@ -52,9 +29,6 @@ echo -e "        EXA_PROBLEM_NAME: \"$PROBLEM_NAME\""
 echo -e "        EXA_PROBLEM_PATH: \"$PROBLEM_PATH\""
 echo -e "        EXASLANG_FILES: \"$EXASLANG\""
 echo -e "        KNOWLEDGE_FILE: \"$KNOWLEDGE\""
-echo -e "    needs:"
-echo -e "        - pipeline: \"$PARENT_PARENT_PIPELINE_ID\""
-echo -e "          job: stage_gen:$PROBLEM_NAME"
 echo
 
 echo -e "bench_pipe:${PROBLEM_NAME}_CUDA:"
@@ -67,7 +41,4 @@ echo -e "        EXA_PROBLEM_PATH: \"$PROBLEM_PATH\""
 echo -e "        EXASLANG_FILES: \"$EXASLANG\""
 echo -e "        KNOWLEDGE_FILE: \"$KNOWLEDGE\""
 echo -e "        PLATFORM_FILE: \"Platform/medusa_QuadroRTX6000.platform\""
-echo -e "    needs:"
-echo -e "        - pipeline: \"$PARENT_PARENT_PIPELINE_ID\""
-echo -e "          job: stage_gen:$PROBLEM_NAME"
 echo
