641d72928dc105866c188dec0db14298bfe92136
zy69guqi <richard.angersbach@fau.de>
2023-02-13 14:00:31 +0100

    Fixes for generating cb stages with generate_cb_yml_stages.sh [skip ci]

--- Benchmark/generate_cb_yml_stages.sh
+++ Benchmark/generate_cb_yml_stages.sh
@@ -2,14 +2,15 @@
 
 # utility script to generate pipeline stages for CB
 
-if [ $# -ne 4 ]; then
-  echo "Wrong number of arguments. Should be ./<script_name>.sh <EXA_PROBLEM_NAME> <EXA_PROBLEM_PATH> <EXASLANG_FILES> <KNOWLEDGE_FILE>"
+if [ $# -ne 5 ]; then
+  echo "Wrong number of arguments. Should be ./<script_name>.sh <EXA_PROBLEM_NAME> <EXA_PROBLEM_PATH> <EXASLANG_FILES> <KNOWLEDGE_FILE> <KNOWLEDGE_FILE_CUDA>"
 fi
 
 PROBLEM_NAME=$1
 PROBLEM_PATH=$2
 EXASLANG=$3
 KNOWLEDGE=$4
+KNOWLEDGE_CUDA=$5
 
 echo -e "bench_gen:$PROBLEM_NAME:"
 echo -e "    stage: bench_gen"
@@ -36,9 +37,9 @@ echo -e "    stage: bench_pipe"
 echo -e "    extends: .cuda_job_template"
 echo -e "    variables:"
 echo -e "        SLURM_NODELIST: \"medusa\""
-echo -e "        EXA_PROBLEM_NAME: \"$PROBLEM_NAME\""
+echo -e "        EXA_PROBLEM_NAME: \"${PROBLEM_NAME}_CUDA\""
 echo -e "        EXA_PROBLEM_PATH: \"$PROBLEM_PATH\""
 echo -e "        EXASLANG_FILES: \"$EXASLANG\""
-echo -e "        KNOWLEDGE_FILE: \"$KNOWLEDGE\""
+echo -e "        KNOWLEDGE_FILE: \"$KNOWLEDGE_CUDA\""
 echo -e "        PLATFORM_FILE: \"Platform/medusa_QuadroRTX6000.platform\""
 echo
