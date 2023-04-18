eff5ba78cccbf95bc8a66e0ee95c7d1ce203dcc7
zy69guqi <richard.angersbach@fau.de>
2023-04-18 11:47:22 +0200

    Use IR_InternalVariableLike trait in simplifications

--- Compiler/src/exastencils/optimization/ir/IR_SimplifyExpression.scala
+++ Compiler/src/exastencils/optimization/ir/IR_SimplifyExpression.scala
@@ -22,7 +22,7 @@ import scala.collection._
 import scala.collection.mutable.{ HashMap, ListBuffer }
 
 import exastencils.base.ir._
-import exastencils.baseExt.ir.IR_InternalVariable
+import exastencils.baseExt.ir.IR_InternalVariableLike
 import exastencils.communication.ir.IR_TempBufferAccess
 import exastencils.core._
 import exastencils.datastructures.Transformation._
@@ -351,11 +351,11 @@ object IR_SimplifyExpression {
           res(IR_Maximum(exprs)) = 1L
         }
 
-      case scalarIV : IR_InternalVariable if scalarIV.resolveDatatype().isInstanceOf[IR_ScalarDatatype] =>
+      case scalarIV : IR_InternalVariableLike if scalarIV.resolveDatatype().isInstanceOf[IR_ScalarDatatype] =>
         res = new HashMap[IR_Expression, Long]()
         res(scalarIV) = 1L
 
-      case anyIV : IR_InternalVariable =>
+      case anyIV : IR_InternalVariableLike =>
         Logger.warn(s"Found non-scalar iv ${ anyIV.prettyprint() } in extractIntegralSumRec")
         res = new HashMap[IR_Expression, Long]()
         res(anyIV) = 1L
