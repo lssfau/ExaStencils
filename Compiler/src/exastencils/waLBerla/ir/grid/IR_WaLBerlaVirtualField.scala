c76724b015827d8696628529ebd6c95da2598c41
zy69guqi <richard.angersbach@fau.de>
2023-04-20 15:46:43 +0200

    Move waLBerla vf access replacement strategy

--- Compiler/src/exastencils/waLBerla/ir/grid/IR_WaLBerlaVirtualField.scala
+++ Compiler/src/exastencils/waLBerla/ir/grid/IR_WaLBerlaVirtualField.scala
@@ -35,38 +35,3 @@ trait IR_WaLBerlaVirtualFieldPerDim extends IR_VirtualFieldPerDim {
   override def knownAliases = vf.knownAliases
   override def localization = vf.localization
 }
-
-
-object IR_WaLBerlaReplaceVirtualFieldAccesses extends DefaultStrategy("Replace vf accesses with waLBerla function calls") {
-  var collector = new IR_StackCollector
-  this.register(collector)
-  this.onBefore = () => this.resetCollectors()
-
-  def inWaLBerlaBlockLoop(collector : IR_StackCollector) = {
-    collector.stack.exists {
-      case _ : IR_WaLBerlaLoopOverBlocks                                                      => true
-      case loop : IR_LoopOverPoints if loop.field.isInstanceOf[IR_WaLBerlaField]              => true
-      case loop : IR_LoopOverPointsInOneFragment if loop.field.isInstanceOf[IR_WaLBerlaField] => true
-      case _                                                                                  => false
-    }
-  }
-
-  // replace vf accesses
-  this += Transformation("Replace", {
-    case _ @ IR_VirtualFieldAccess(IR_VF_CellCenterPerDim(lvl, domain, dim), idx, fragIdx) if inWaLBerlaBlockLoop(collector) =>
-      IR_VirtualFieldAccess(IR_WaLBerlaCellCenterPerDim(lvl, domain, dim), idx, fragIdx)
-    case _ @ IR_VirtualFieldAccess(IR_VF_CellCenterAsVec(lvl, domain), idx, fragIdx) if inWaLBerlaBlockLoop(collector) =>
-      IR_VirtualFieldAccess(IR_WaLBerlaCellCenterAsVec(lvl, domain), idx, fragIdx)
-
-    case _ @ IR_VirtualFieldAccess(IR_VF_CellWidthPerDim(lvl, domain, dim), idx, fragIdx) if inWaLBerlaBlockLoop(collector) =>
-      IR_VirtualFieldAccess(IR_WaLBerlaCellWidthPerDim(lvl, domain, dim), idx, fragIdx)
-    case _ @ IR_VirtualFieldAccess(IR_VF_CellWidthAsVec(lvl, domain), idx, fragIdx) if inWaLBerlaBlockLoop(collector) =>
-      IR_VirtualFieldAccess(IR_WaLBerlaCellWidthAsVec(lvl, domain), idx, fragIdx)
-
-    // boundary positions snap from cell center to node position -> introduce waLBerla node positions
-    case _ @ IR_VirtualFieldAccess(IR_VF_NodePositionPerDim(lvl, domain, dim), idx, fragIdx) if inWaLBerlaBlockLoop(collector) =>
-      IR_VirtualFieldAccess(IR_WaLBerlaNodePositionPerDim(lvl, domain, dim), idx, fragIdx)
-    case _ @ IR_VirtualFieldAccess(IR_VF_NodePositionAsVec(lvl, domain), idx, fragIdx) if inWaLBerlaBlockLoop(collector) =>
-      IR_VirtualFieldAccess(IR_WaLBerlaNodePositionAsVec(lvl, domain), idx, fragIdx)
-  })
-}
