3774e1d0a44d9e397ca905270c841cdc33730f92
zy69guqi <richard.angersbach@fau.de>
2023-07-10 17:21:46 +0200

    Add mechanism to automatically measure communication, boundary handling, etc via knowledge flags

--- Compiler/src/exastencils/base/ir/IR_FutureFunction.scala
+++ Compiler/src/exastencils/base/ir/IR_FutureFunction.scala
@@ -19,6 +19,8 @@
 package exastencils.base.ir
 
 import exastencils.datastructures.Transformation.Output
+import exastencils.base.ir.IR_ImplicitConversion._
+import exastencils.timing.ir._
 
 /// IR_FutureFunction
 
@@ -38,15 +40,50 @@ trait IR_FutureFunction extends IR_FunctionLike with IR_Expandable {
   def generateFct() : IR_Function
 }
 
+/// IR_FutureFunctionWithTiming
+
+trait IR_FutureFunctionWithTiming extends IR_FutureFunction {
+
+  def automaticTimingCategory : IR_AutomaticFunctionTimingCategory.Access
+
+  override def expand() : Output[IR_Function] = {
+    var genFct = generateFct()
+
+    if (IR_AutomaticFunctionTimingCategory.categoryEnabled(automaticTimingCategory)) {
+      val timer = IR_IV_Timer(s"autoTime_${automaticTimingCategory.toString}_$name")
+
+      timer.annotate(IR_AutomaticFunctionTimingCategory.ANNOT, automaticTimingCategory)
+
+      genFct.body.prepend(IR_FunctionCall(IR_StartTimer().name, timer))
+      genFct.body.append(IR_FunctionCall(IR_StopTimer().name, timer))
+    }
+
+    genFct
+  }
+}
+
 /// IR_FuturePlainFunction
 
 trait IR_FuturePlainFunction extends IR_FutureFunction {
   override def generateFct() : IR_PlainFunction
 }
 
+/// IR_FuturePlainFunctionWithTiming
+
+trait IR_FuturePlainFunctionWithTiming extends IR_FutureFunctionWithTiming {
+  override def generateFct() : IR_PlainFunction
+}
+
 /// IR_FutureLeveledFunction
 
 trait IR_FutureLeveledFunction extends IR_FutureFunction {
+  def level : Int
+  override def generateFct() : IR_LeveledFunction
+}
+
+/// IR_FutureLeveledFunctionWithTiming
+
+trait IR_FutureLeveledFunctionWithTiming extends IR_FutureFunctionWithTiming {
   def level : Int
   override def generateFct() : IR_LeveledFunction
 }
