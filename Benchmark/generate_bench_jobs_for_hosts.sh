f3927373741e7ec41b0836adb3b089310b4770a6
zy69guqi <richard.angersbach@fau.de>
2023-07-14 13:22:14 +0200

    Try refactoring CB pipeline design by generating more pipelines and yml stages

--- Benchmark/generate_bench_jobs_for_hosts.sh
+++ Benchmark/generate_bench_jobs_for_hosts.sh
@@ -5,23 +5,36 @@ if [ $# -ne 2 ]; then
 fi
 
 # get idle hosts (and filter out drained hosts)
-HOSTLIST=$(echo "$(sinfo -t idle -h --partition=work -o "%n"; sinfo -t drain -h -o "%n")" | sort | uniq -u)
-for HOST in ${HOSTLIST}; do
-  if [[ "$HOST" != "aurora1" && "$HOST" != "warmup" && "$HOST" != "applem1studio" ]]; then
-    echo "stages:"
-    echo "    - bench_pipe"
-    echo ""
-    echo "$(<.benchmark_templates.yml)"
-    echo ""
-    echo "benchmark-$1-$HOST:"
-    echo -e "    stage: bench_pipe"
-    echo -e "    extends: .bench_pipe_template"
-    echo -e "    variables:"
-    echo -e "        EXA_PROBLEM_PATH: "$2""
-    echo -e "        SLURM_NODELIST: "$HOST""
-    echo -e "    needs:"
-    echo -e "        - pipeline: \"$PARENT_PIPELINE_ID\""
-    echo -e "          job: bench_gen:$1"
-    echo
+HOST_LIST=$(echo "$(sinfo -t idle -h --partition=work -o "%n"; sinfo -t drain -h -o "%n")" | sort | uniq -u)
+HOST_FILTER='aurora1 warmup applem1studio'
+
+# include template
+echo "$(<.benchmark_templates.yml)"
+
+# print out stages
+echo "stages:"
+for HOST in ${HOST_LIST[@]}; do
+  if [[ " $HOST_FILTER " =~ .*\ $HOST\ .* ]]; then
+    continue
   fi
+  echo "    - bench_pipe_$1_$HOST"
+done
+
+# generate pipeline
+for HOST in ${HOST_LIST[@]}; do
+  if [[ " $HOST_FILTER " =~ .*\ $HOST\ .* ]]; then
+    continue
+  fi
+
+  echo ""
+  echo "benchmark-$1-$HOST:"
+  echo -e "    stage: bench_pipe_$1_$HOST"
+  echo -e "    extends: .bench_pipe_template"
+  echo -e "    variables:"
+  echo -e "        EXA_PROBLEM_PATH: "$2""
+  echo -e "        SLURM_NODELIST: "$HOST""
+  echo -e "    needs:"
+  echo -e "        - pipeline: \$PARENT_PIPELINE_ID"
+  echo -e "          job: bench_gen:$1"
+  echo
 done
--- Benchmark/generate_cb_yml_stages.sh
+++ Benchmark/generate_cb_yml_stages.sh
@@ -2,48 +2,99 @@
 
 # utility script to generate pipeline stages for CB
 
-if [ $# -ne 5 ]; then
-  echo "Wrong number of arguments. Should be ./<script_name>.sh <EXA_PROBLEM_NAME> <EXA_PROBLEM_PATH> <EXASLANG_FILES> <KNOWLEDGE_FILE> <KNOWLEDGE_FILE_CUDA>"
-fi
+## generator functions
 
-PROBLEM_NAME=$1
-PROBLEM_PATH=$2
-EXASLANG=$3
-KNOWLEDGE=$4
-KNOWLEDGE_CUDA=$5
+function generate_pipeline_stages_for_app() {
+  if [ $# -ne 5 ]; then
+    echo "Wrong number of arguments. Should be ./<script_name>.sh <EXA_PROBLEM_NAME> <EXA_PROBLEM_PATH> <EXASLANG_FILES> <KNOWLEDGE_FILE> <KNOWLEDGE_FILE_CUDA>"
+  fi
 
-echo -e "bench_gen:$PROBLEM_NAME:"
-echo -e "    stage: bench_gen"
-echo -e "    extends: .gen_bench_job_template"
-echo -e "    variables:"
-echo -e "        EXA_PROBLEM_NAME: \"$PROBLEM_NAME\""
-echo -e "        EXA_PROBLEM_PATH: \"$PROBLEM_PATH\""
-echo -e "        EXASLANG_FILES: \"$EXASLANG\""
-echo -e "        KNOWLEDGE_FILE: \"$KNOWLEDGE\""
+  PROBLEM_NAME=$1
+  PROBLEM_PATH=$2
+  EXASLANG=$3
+  KNOWLEDGE=$4
+  KNOWLEDGE_CUDA=$5
+
+  echo -e "    - bench_gen_$PROBLEM_NAME"
+  echo -e "    - bench_pipe_$PROBLEM_NAME"
+  echo -e "    - bench_pipe_${PROBLEM_NAME}_CUDA"
+}
+
+function generate_pipeline_jobs_for_app() {
+  if [ $# -ne 5 ]; then
+    echo "Wrong number of arguments. Should be ./<script_name>.sh <EXA_PROBLEM_NAME> <EXA_PROBLEM_PATH> <EXASLANG_FILES> <KNOWLEDGE_FILE> <KNOWLEDGE_FILE_CUDA>"
+  fi
+
+  PROBLEM_NAME=$1
+  PROBLEM_PATH=$2
+  EXASLANG=$3
+  KNOWLEDGE=$4
+  KNOWLEDGE_CUDA=$5
+
+  echo -e "bench_gen:$PROBLEM_NAME:"
+  echo -e "    stage: bench_gen_$PROBLEM_NAME"
+  echo -e "    extends: .gen_bench_job_template"
+  echo -e "    variables:"
+  echo -e "        EXA_PROBLEM_NAME: \"$PROBLEM_NAME\""
+  echo -e "        EXA_PROBLEM_PATH: \"$PROBLEM_PATH\""
+  echo -e "        EXASLANG_FILES: \"$EXASLANG\""
+  echo -e "        KNOWLEDGE_FILE: \"$KNOWLEDGE\""
+  echo
+
+  echo -e "bench_pipe:$PROBLEM_NAME:"
+  echo -e "    stage: bench_pipe_$PROBLEM_NAME"
+  echo -e "    trigger:"
+  echo -e "        include:"
+  echo -e "            - artifact: Benchmark/$PROBLEM_NAME-child-pipe.yml"
+  echo -e "              job: bench_gen:$PROBLEM_NAME"
+  echo -e "        strategy: depend"
+  echo -e "    variables:"
+  echo -e "        EXA_PROBLEM_NAME: \"$PROBLEM_NAME\""
+  echo -e "        EXA_PROBLEM_PATH: \"$PROBLEM_PATH\""
+  echo -e "        EXASLANG_FILES: \"$EXASLANG\""
+  echo -e "        KNOWLEDGE_FILE: \"$KNOWLEDGE\""
+  echo
+
+  echo -e "bench_pipe:${PROBLEM_NAME}_CUDA:"
+  echo -e "    stage: bench_pipe_${PROBLEM_NAME}_CUDA"
+  echo -e "    extends: .cuda_job_template"
+  echo -e "    variables:"
+  echo -e "        SLURM_NODELIST: \"medusa\""
+  echo -e "        EXA_PROBLEM_NAME: \"${PROBLEM_NAME}_CUDA\""
+  echo -e "        EXA_PROBLEM_PATH: \"$PROBLEM_PATH\""
+  echo -e "        EXASLANG_FILES: \"$EXASLANG\""
+  echo -e "        KNOWLEDGE_FILE: \"$KNOWLEDGE_CUDA\""
+  echo -e "        PLATFORM_FILE: \"Platform/medusa_QuadroRTX6000.platform\""
+  echo
+}
+
+## generate yml file
+
+echo -e "variables:"
+echo -e "    GIT_STRATEGY: clone"
+echo -e "    GIT_SUBMODULE_STRATEGY: recursive"
 echo
 
-echo -e "bench_pipe:$PROBLEM_NAME:"
-echo -e "    stage: bench_pipe"
-echo -e "    trigger:"
-echo -e "        include:"
-echo -e "            - artifact: Benchmark/$PROBLEM_NAME-pipe.yml"
-echo -e "              job: bench_gen:$PROBLEM_NAME"
-echo -e "        strategy: depend"
-echo -e "    variables:"
-echo -e "        EXA_PROBLEM_NAME: \"$PROBLEM_NAME\""
-echo -e "        EXA_PROBLEM_PATH: \"$PROBLEM_PATH\""
-echo -e "        EXASLANG_FILES: \"$EXASLANG\""
-echo -e "        KNOWLEDGE_FILE: \"$KNOWLEDGE\""
+echo -e "include: 'Benchmark/.benchmark_templates.yml'"
 echo
 
-echo -e "bench_pipe:${PROBLEM_NAME}_CUDA:"
-echo -e "    stage: bench_pipe"
-echo -e "    extends: .cuda_job_template"
-echo -e "    variables:"
-echo -e "        SLURM_NODELIST: \"medusa\""
-echo -e "        EXA_PROBLEM_NAME: \"${PROBLEM_NAME}_CUDA\""
-echo -e "        EXA_PROBLEM_PATH: \"$PROBLEM_PATH\""
-echo -e "        EXASLANG_FILES: \"$EXASLANG\""
-echo -e "        KNOWLEDGE_FILE: \"$KNOWLEDGE_CUDA\""
-echo -e "        PLATFORM_FILE: \"Platform/medusa_QuadroRTX6000.platform\""
+### generator func args
+
+five_point_args=("5pt_Jac_2D" "FivePointStencil" "5pt_Jac_Cell.exa4" "5pt_Jac_Cell.knowledge" "5pt_Jac_Cell_CUDA.knowledge")
+opt_flow_args=("2D_FD_OptFlow" "OptFlow2D" "2D_FD_OptFlow.exa4" "2D_FD_OptFlow.knowledge" "2D_FD_OptFlow_CUDA.knowledge")
+poisson3D_args=("3D_FD_Poisson" "Poisson3D" "3D_FD_Poisson_fromL4.exa4" "3D_FD_Poisson_fromL4.knowledge" "3D_FD_Poisson_fromL4_CUDA.knowledge")
+
+
+### generate stage names
+
+echo -e "stages:"
+generate_pipeline_stages_for_app "${five_point_args[@]}"
+generate_pipeline_stages_for_app "${opt_flow_args[@]}"
+generate_pipeline_stages_for_app "${poisson3D_args[@]}"
 echo
+
+### generate job names
+
+generate_pipeline_jobs_for_app "${five_point_args[@]}"
+generate_pipeline_jobs_for_app "${opt_flow_args[@]}"
+generate_pipeline_jobs_for_app "${poisson3D_args[@]}"
