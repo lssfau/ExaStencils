e986e6523638d72945f2d76f8fef1f1fa5a018d6
zy69guqi <richard.angersbach@fau.de>
2023-07-27 19:10:22 +0200

    Make sure that functions for variable field sizes are generated when non-fixed wb fields are used

--- Compiler/src/exastencils/config/Knowledge.scala
+++ Compiler/src/exastencils/config/Knowledge.scala
@@ -1000,6 +1000,7 @@ object Knowledge {
     // waLBerla
     Constraints.condError(waLBerla_generateInterface && dimensionality < 2, "waLBerla coupling is only supported for 2D/3D simulations.")
     Constraints.condEnsureValue(waLBerla_useFixedLayoutsFromExa, true, cuda_enabled && waLBerla_generateInterface, "CUDA support for waLBerla codegen is only applicable with fixed field layouts (waLBerla_useFixedLayoutsFromExa = true).")
+    Constraints.condEnsureValue(data_genVariableFieldSizes, true, !waLBerla_useFixedLayoutsFromExa && waLBerla_generateInterface, "Enable 'data_genVariableFieldSizes' when waLberla fields without fixed layouts are used.")
     Constraints.condError(!waLBerla_useGridPartFromExa && waLBerla_createCartComm, "Knowledge flag 'waLBerla_createCartComm' is only available with Exa grid partitioning (waLBerla_useGridPartFromExa = true).")
 
     Constraints.condEnsureValue(waLBerla_useRefinement, true, waLBerla_refinementLevels > 0, "Flag 'waLBerla_useRefinement' must be enabled when 'waLBerla_refinementLevels' > 0")
