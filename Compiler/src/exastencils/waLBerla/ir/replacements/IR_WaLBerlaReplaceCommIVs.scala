bedc287a3bcbc7087221de6492973c96194300a9
zy69guqi <richard.angersbach@fau.de>
2023-09-28 20:35:21 +0200

    Add guards to walberla comm IV replacement strat

--- Compiler/src/exastencils/waLBerla/ir/replacements/IR_WaLBerlaReplaceCommIVs.scala
+++ Compiler/src/exastencils/waLBerla/ir/replacements/IR_WaLBerlaReplaceCommIVs.scala
@@ -9,21 +9,21 @@ import exastencils.waLBerla.ir.communication._
 object IR_WaLBerlaReplaceCommIVs extends IR_WaLBerlaReplacementStrategy("Replace comm IV accesses to wb counterparts") {
 
   this += Transformation("Replace", {
-    case _ @ IR_IV_CommBuffer(field, send, size, neighIdx, concurrencyId, indexOfRefinedNeighbor, fragmentIdx)        =>
+    case _ @ IR_IV_CommBuffer(field, send, size, neighIdx, concurrencyId, indexOfRefinedNeighbor, fragmentIdx) if inWaLBerlaScope(collector)        =>
       IR_WaLBerlaCommBuffer(field, send, size, neighIdx, concurrencyId, indexOfRefinedNeighbor, fragmentIdx)
-    case _ @ IR_IV_CommBufferBasePtr(field, send, size, neighIdx, concurrencyId, indexOfRefinedNeighbor, fragmentIdx) =>
+    case _ @ IR_IV_CommBufferBasePtr(field, send, size, neighIdx, concurrencyId, indexOfRefinedNeighbor, fragmentIdx) if inWaLBerlaScope(collector) =>
       IR_WaLBerlaCommBufferBasePtr(field, send, size, neighIdx, concurrencyId, indexOfRefinedNeighbor, fragmentIdx)
-    case _ @ IR_IV_CommBufferIterator(field, send, neighIdx, concurrencyId, indexOfRefinedNeighbor, fragmentIdx)      =>
+    case _ @ IR_IV_CommBufferIterator(field, send, neighIdx, concurrencyId, indexOfRefinedNeighbor, fragmentIdx) if inWaLBerlaScope(collector)      =>
       IR_WaLBerlaCommBufferIterator(field, send, neighIdx, concurrencyId, indexOfRefinedNeighbor, fragmentIdx)
 
-    case _ @ MPI_Request(field, send, neighIdx, concurrencyId, indexOfRefinedNeighbor, fragmentIdx) =>
+    case _ @ MPI_Request(field, send, neighIdx, concurrencyId, indexOfRefinedNeighbor, fragmentIdx) if inWaLBerlaScope(collector) =>
       IR_WaLBerlaMPIRequest(field, send, neighIdx, concurrencyId, indexOfRefinedNeighbor, fragmentIdx)
-    case _ @ MPI_RequestNoField(send, neighIdx, fragmentIdx)                                        =>
+    case _ @ MPI_RequestNoField(send, neighIdx, fragmentIdx) if inWaLBerlaScope(collector)                                        =>
       IR_WaLBerlaMPIRequestNoField(send, neighIdx, fragmentIdx)
 
-    case _ @ IR_IV_RemoteReqOutstanding(field, send, neighIdx, concurrencyId, indexOfRefinedNeighbor, fragmentIdx) =>
+    case _ @ IR_IV_RemoteReqOutstanding(field, send, neighIdx, concurrencyId, indexOfRefinedNeighbor, fragmentIdx) if inWaLBerlaScope(collector) =>
       IR_WaLBerlaRemoteReqOutstanding(field, send, neighIdx, concurrencyId, indexOfRefinedNeighbor, fragmentIdx)
-    case _ @ IR_IV_RemoteReqOutstandingNoField(send, neighIdx, concurrencyId, indexOfRefinedNeighbor, fragmentIdx) =>
+    case _ @ IR_IV_RemoteReqOutstandingNoField(send, neighIdx, concurrencyId, indexOfRefinedNeighbor, fragmentIdx) if inWaLBerlaScope(collector) =>
       IR_WaLBerlaRemoteReqOutstandingNoField(send, neighIdx, concurrencyId, indexOfRefinedNeighbor, fragmentIdx)
 
   }, recursive = false)
